3cf520ff53dc1207a2298728141ab009b239a6cd
Kirill Taran <kirill.taran@hotmail.com>
2019-04-17 13:17:04 +0300

    Add project files.

---
+++ Api/Controllers/HotelsV1Controller.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HappyTravel.Edo.Api.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("api/{v:apiVersion}/hotels")]
+    [Produces("application/json")]
+    public class HotelsV1Controller : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            throw new ArgumentNullException();
+        }
+    }
+}
---
+++ Api/Startup.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using FloxDc.Bento.Responses.Middleware;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
+using Swashbuckle.AspNetCore.Swagger;
+
+namespace HappyTravel.Edo.Api
+{
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddApiVersioning(options =>
+            {
+                options.AssumeDefaultVersionWhenUnspecified = false;
+                options.DefaultApiVersion = new ApiVersion(1, 0);
+                options.ReportApiVersions = true;
+            });
+
+            services.AddMvcCore()
+                .AddAuthorization()
+                .AddCors()
+                .AddControllersAsServices()
+                .AddFormatterMappings()
+                .AddJsonFormatters()
+                .AddApiExplorer()
+                .AddCacheTagHelper()
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+
+            services.AddResponseCompression();
+
+            services.AddSwaggerGen(options =>
+            {
+                options.SwaggerDoc("v1.0", new Info {Title = "HappyTravel.com API", Version = "v1.0" });
+
+                var xmlCommentsFileName = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+                var xmlCommentsFilePath = Path.Combine(AppContext.BaseDirectory, xmlCommentsFileName);
+                options.IncludeXmlComments(xmlCommentsFilePath);
+            });
+        }
+
+
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            app.UseBentoExceptionHandler(env.IsProduction());
+            
+            app.UseSwagger();
+            app.UseSwaggerUI(options =>
+            {
+                options.SwaggerEndpoint("/swagger/v1.0/swagger.json", "HappyTravel.com API");
+                options.RoutePrefix = string.Empty;
+            });
+
+            app.UseResponseCompression();
+
+            app.UseMvc();
+        }
+    }
+}
