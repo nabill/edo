7ff8c45b501926fc96412a263ff366d16166716b
Kirill Taran <kirill.taran@hotmail.com>
2019-08-13 16:34:23 +0300

    Removed netstorming-specific fields from a booking request

--- Api/Models/Bookings/HotelBookingRequest.cs
+++ Api/Models/Bookings/HotelBookingRequest.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using HappyTravel.Edo.Api.Models.Availabilities;
 using HappyTravel.Edo.Api.Models.Hotels;
 using Newtonsoft.Json;
 
@@ -8,49 +10,80 @@ namespace HappyTravel.Edo.Api.Models.Bookings
     public readonly struct HotelBookingRequest
     {
         [JsonConstructor]
-        public HotelBookingRequest(string searchNumber, string nationality, 
-            DateTime checkInDate, DateTime checkOutDate, string cityCode, bool availableOnly,
-            string hotelId, string agreement, string referenceCode, List<string> responses,
-            List<BookingRoomDetails> roomDetails, List<HotelFeature> features)
+        public HotelBookingRequest(string availabilityId, string nationality, string residency,
+            DateTime checkInDate, DateTime checkOutDate, string hotelId, string tariffCode,
+            List<BookingRoomDetails> roomDetails, List<HotelFeature> features, bool availableOnly = true)
         {
-            SearchNumber = searchNumber;
-            Nationality = nationality;
+            AvailabilityId = availabilityId;
+            AvailableOnly = availableOnly;
             CheckInDate = checkInDate;
             CheckOutDate = checkOutDate;
-            CityCode = cityCode;
-            AvailableOnly = availableOnly;
-			
             HotelId = hotelId;
-            Agreement = agreement;
-			
-            ReferenceCode = referenceCode;
-            Responses = responses ?? new List<string>(0);
+            Nationality = nationality;
+            Residency = residency;
+            TariffCode = tariffCode;
+
             RoomDetails = roomDetails ?? new List<BookingRoomDetails>(0);
             Features = features ?? new List<HotelFeature>(0);
         }
-		
-        public string SearchNumber { get; }
-		
-        public string Nationality { get; }
-		
-        public DateTime CheckInDate { get; }
 
-        public DateTime CheckOutDate { get; }
-		
-        public string CityCode { get; }
-		
+
+        /// <summary>
+        ///     Availability ID obtained from an <see cref="AvailabilityResponse" />.
+        /// </summary>
+        [Required]
+        public string AvailabilityId { get; }
+
+        /// <summary>
+        ///     This indicates the system to reject the request when an accommodation has been booked by some one else between
+        ///     availability and booking requests. Default is true.
+        /// </summary>
         public bool AvailableOnly { get; }
 
+        /// <summary>
+        ///     The check-in date.
+        /// </summary>
+        [Required]
+        public DateTime CheckInDate { get; }
+
+        /// <summary>
+        ///     The check-out date.
+        /// </summary>
+        [Required]
+        public DateTime CheckOutDate { get; }
+
+        /// <summary>
+        ///     The ID of a booked accommodation.
+        /// </summary>
+        [Required]
         public string HotelId { get; }
 
-        public string Agreement { get; }
+        /// <summary>
+        ///     The nationality of a main passenger.
+        /// </summary>
+        [Required]
+        public string Nationality { get; }
 
-        public string ReferenceCode { get; }
+        /// <summary>
+        ///     The residency of a main passenger.
+        /// </summary>
+        [Required]
+        public string Residency { get; }
 
-        public List<string> Responses { get; }
+        /// <summary>
+        ///     Tariff code from an agreements section of availability response.
+        /// </summary>
+        [Required]
+        public string TariffCode { get; }
 
+        /// <summary>
+        ///     Room details from an availability response.
+        /// </summary>
         public List<BookingRoomDetails> RoomDetails { get; }
 
+        /// <summary>
+        ///     The selected additional accommodation features, if any.
+        /// </summary>
         public List<HotelFeature> Features { get; }
     }
 }
