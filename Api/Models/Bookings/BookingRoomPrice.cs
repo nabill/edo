e10421c5abd941e3fe2d165705201e88bf54f729
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-08-27 13:29:04 +0500

    BookingRoom price deserialization

--- Api/Models/Bookings/BookingRoomPrice.cs
+++ Api/Models/Bookings/BookingRoomPrice.cs
@@ -1,7 +1,10 @@
-﻿namespace HappyTravel.Edo.Api.Models.Bookings
+﻿using Newtonsoft.Json;
+
+namespace HappyTravel.Edo.Api.Models.Bookings
 {
     public readonly struct BookingRoomPrice
     {
+        [JsonConstructor]
         public BookingRoomPrice(decimal roomPrice, decimal extraBedPrice = 0, decimal cotPrice = 0)
         {
             Price = roomPrice;
