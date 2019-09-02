53cc6380aa762c2f099d0f5d51edc2fce81c7c7b
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-09-02 17:54:04 +0500

    Register regular customers in registration service

--- Api/Services/Customers/IRegistrationService.cs
+++ Api/Services/Customers/IRegistrationService.cs
@@ -7,5 +7,6 @@ namespace HappyTravel.Edo.Api.Services.Customers
     public interface IRegistrationService
     {
         ValueTask<Result> RegisterMasterCustomer(CompanyRegistrationInfo company, CustomerRegistrationInfo masterCustomer, string externalIdentity);
+        Task<Result> RegisterRegularCustomer(CustomerRegistrationInfo requestCustomerRegistrationInfo, int companyId, string externalIdentity);
     }
 }
--- Api/Services/Customers/RegistrationService.cs
+++ Api/Services/Customers/RegistrationService.cs
@@ -1,9 +1,11 @@
+using System.Linq;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
 using HappyTravel.Edo.Api.Models.Customers;
 using HappyTravel.Edo.Common.Enums;
 using HappyTravel.Edo.Data;
 using HappyTravel.Edo.Data.Customers;
+using Microsoft.EntityFrameworkCore;
 
 namespace HappyTravel.Edo.Api.Services.Customers
 {
@@ -39,7 +41,41 @@ namespace HappyTravel.Edo.Api.Services.Customers
             await _context.SaveChangesAsync();
             return Result.Ok();
         }
-        
+
+        public async Task<Result> RegisterRegularCustomer(CustomerRegistrationInfo requestCustomerRegistrationInfo,
+            int companyId, string externalIdentity)
+        {
+            return await _customerService.Create(requestCustomerRegistrationInfo, externalIdentity)
+                .Ensure(IsRelationExists, "Company relations is already exist")
+                .Ensure(IsNotExistingCompany, "Company does not exist")
+                .OnSuccess(AddCompanyRelation);
+
+            Task<bool> IsRelationExists(Customer customer)
+            {
+                return _context.CustomerCompanyRelations
+                    .Where(cr => cr.CustomerId == customer.Id)
+                    .Where(cr => cr.CompanyId == companyId)
+                    .AnyAsync();
+            }
+            
+            async Task<bool> IsNotExistingCompany(Customer customer)
+            {
+                return !await _context.Companies
+                    .AnyAsync(c => c.Id == companyId);
+            }
+
+            Task<int> AddCompanyRelation(Customer customer)
+            {
+                _context.CustomerCompanyRelations.Add(new CustomerCompanyRelation
+                {
+                    Type = CustomerCompanyRelationTypes.Regular,
+                    CompanyId = companyId,
+                    CustomerId = customer.Id
+                });
+                return _context.SaveChangesAsync();
+            }
+        }
+
         private readonly ICompanyService _companyService;
         private readonly EdoContext _context;
         private readonly ICustomerService _customerService;
