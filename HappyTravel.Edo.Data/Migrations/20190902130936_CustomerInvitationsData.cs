6c56f8539d1082d76e12bb7f91be2a31c4272516
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-09-02 19:13:07 +0500

    Customer invitation migration

---
+++ HappyTravel.Edo.Data/Migrations/20190902130936_CustomerInvitationsData.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class CustomerInvitationsData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CustomerInvitations",
+                columns: table => new
+                {
+                    Code = table.Column<string>(nullable: false),
+                    Data = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false),
+                    IsAccepted = table.Column<bool>(nullable: false, defaultValue: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CustomerInvitations", x => x.Code);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CustomerInvitations");
+        }
+    }
+}
