a828c8b8ed7e6340988c27462e9eacf7ff6b9fb6
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-09-12 17:25:51 +0500

    Invitation type added migration

---
+++ HappyTravel.Edo.Data/Migrations/20190912122455_InvitationTypeAdded.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class InvitationTypeAdded : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "InvitationType",
+                table: "UserInvitations",
+                nullable: false,
+                defaultValue: 1);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvitationType",
+                table: "UserInvitations");
+        }
+    }
+}
