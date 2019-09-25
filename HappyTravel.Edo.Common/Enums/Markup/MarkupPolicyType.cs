bdcee17e4e8ea19ab439b7030a80c478fcdc4abc
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-09-25 11:07:22 +0500

    Serializer attrubutes for markup enum values

--- HappyTravel.Edo.Common/Enums/Markup/MarkupPolicyType.cs
+++ HappyTravel.Edo.Common/Enums/Markup/MarkupPolicyType.cs
@@ -1,5 +1,9 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace HappyTravel.Edo.Common.Enums.Markup
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum MarkupPolicyType
     {
         NotSpecified = 0,
