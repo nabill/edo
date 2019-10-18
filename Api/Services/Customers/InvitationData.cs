7d1bc5478d87eee49e33952fbb3bd374384d773f
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-10-18 12:40:22 +0500

    Adding user mail to invitation

--- Api/Services/Customers/InvitationData.cs
+++ Api/Services/Customers/InvitationData.cs
@@ -3,5 +3,6 @@ namespace HappyTravel.Edo.Api.Services.Customers
     public class InvitationData
     {
         public string InvitationCode { get; set; }
+        public string UserMail { get; set; }
     }
 }
