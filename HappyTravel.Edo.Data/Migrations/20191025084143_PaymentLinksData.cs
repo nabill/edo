ac720fe031ce66656b3c25f0fdd0b2a7eb182baf
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-10-25 13:42:54 +0500

    Added payment links table

---
+++ HappyTravel.Edo.Data/Migrations/20191025084143_PaymentLinksData.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class PaymentLinksData : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentLinks",
+                columns: table => new
+                {
+                    Code = table.Column<string>(nullable: false),
+                    Email = table.Column<string>(nullable: true),
+                    Currency = table.Column<string>(nullable: false),
+                    Facility = table.Column<string>(nullable: false),
+                    Price = table.Column<decimal>(nullable: false),
+                    Comment = table.Column<string>(nullable: true),
+                    IsPaid = table.Column<bool>(nullable: false),
+                    Created = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentLinks", x => x.Code);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentLinks");
+        }
+    }
+}
