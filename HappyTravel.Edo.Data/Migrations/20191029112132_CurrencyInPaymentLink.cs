e733b77f2c9799b8dc3cf9836ed43998900675df
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-10-29 16:22:40 +0500

    Payment links currencies migration

---
+++ HappyTravel.Edo.Data/Migrations/20191029112132_CurrencyInPaymentLink.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class CurrencyInPaymentLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "Currency",
+                table: "PaymentLinks",
+                nullable: false,
+                oldClrType: typeof(string));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Currency",
+                table: "PaymentLinks",
+                nullable: false,
+                oldClrType: typeof(int));
+        }
+    }
+}
