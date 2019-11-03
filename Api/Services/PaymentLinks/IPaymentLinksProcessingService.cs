ee4b2d8d19f3504ba6c4ed04f8dd8a6dacf160ad
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-11-03 11:38:29 +0500

    Renamings after code review + storing payment response when response is not 3DSecure

--- Api/Services/PaymentLinks/IPaymentLinksProcessingService.cs
+++ Api/Services/PaymentLinks/IPaymentLinksProcessingService.cs
@@ -9,7 +9,7 @@ namespace HappyTravel.Edo.Api.Services.PaymentLinks
     public interface IPaymentLinksProcessingService
     {
         Task<Result<PaymentResponse>> Pay(string code, string token, string ip, string languageCode);
-        Task<Result<PaymentResponse>> ProcessPaymentResponse(string code, JObject value);
+        Task<Result<PaymentResponse>> ProcessResponse(string code, JObject value);
         Task<Result<string>> CalculateSignature(string code, string languageCode);
     }
 }
