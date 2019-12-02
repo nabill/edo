9cecc8532f9ffbd6af5053e1ea0fdf6d59b2c471
hasmik <hasmik.harutyunyan@happyhotel.com>
2019-12-02 17:43:05 +0400

    fix

--- Api/Infrastructure/CustomerRegistrationOptions.cs
+++ Api/Infrastructure/CustomerRegistrationOptions.cs
@@ -5,7 +5,6 @@ namespace HappyTravel.Edo.Api.Infrastructure
     public class CustomerRegistrationNotificationOptions
     {
         public string MasterCustomerMailTemplateId { get; set; }
-
         public string RegularCustomerMailTemplateId { get; set; }
         public List<string> AdministratorsEmails {get;set;}
     }
