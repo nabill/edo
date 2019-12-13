d7da8c3658a479be2a472241c38eb5050941e663
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-12-13 13:59:17 +0500

    Changed EdoContextFactory to work with vault

--- HappyTravel.Edo.Data/EdoContextFactory.cs
+++ HappyTravel.Edo.Data/EdoContextFactory.cs
@@ -1,6 +1,13 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using HappyTravel.VaultClient;
+using HappyTravel.VaultClient.Extensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace HappyTravel.Edo.Data
 {
@@ -9,15 +16,54 @@ namespace HappyTravel.Edo.Data
         public EdoContext CreateDbContext(string[] args)
         {
             var dbContextOptions = new DbContextOptionsBuilder<EdoContext>();
-            var connectionString = string.Format("Server={0};Port={1};Database=edo;Userid={2};Password={3};",
-                Environment.GetEnvironmentVariable("CS_EDO_HOST"),
-                Environment.GetEnvironmentVariable("CS_EDO_PORT"),
-                Environment.GetEnvironmentVariable("CS_EDO_USERID"),
-                Environment.GetEnvironmentVariable("CS_EDO_PASSWORD"));
-
-            dbContextOptions.UseNpgsql(connectionString, builder => builder.UseNetTopologySuite());
+            dbContextOptions.UseNpgsql(GetConnectionString(), builder => builder.UseNetTopologySuite());
 
             return new EdoContext(dbContextOptions.Options);
         }
+
+
+        private static string GetConnectionString()
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location))
+                .AddJsonFile("appsettings.json", false, true)
+                .Build();
+
+            var dbOptions = GetDbOptions(configuration);
+
+            return string.Format(ConnectionStringTemplate,
+                dbOptions["host"],
+                dbOptions["port"],
+                dbOptions["userId"],
+                dbOptions["password"]);
+        }
+
+
+        private static Dictionary<string, string> GetDbOptions(IConfiguration configuration)
+        {
+            using (var vaultClient = CreateVaultClient(configuration))
+            {
+                vaultClient.Login(Environment.GetEnvironmentVariable(configuration["Vault:Token"])).Wait();
+                return vaultClient.Get(configuration["EDO:Database:Options"]).Result;
+            }
+        }
+
+
+        private static IVaultClient CreateVaultClient(IConfiguration configuration)
+        {
+            var services = new ServiceCollection();
+            services.AddVaultClient(o =>
+            {
+                o.Engine = configuration["Vault:Engine"];
+                o.Role = configuration["Vault:Role"];
+                o.Url = new Uri(Environment.GetEnvironmentVariable(configuration["Vault:Endpoint"]), UriKind.Absolute);
+            });
+            var serviceProvider = services.BuildServiceProvider();
+
+            return serviceProvider.GetRequiredService<IVaultClient>();
+        }
+
+
+        private const string ConnectionStringTemplate = "Server={0};Port={1};Database=edo;Userid={2};Password={3};";
     }
 }
