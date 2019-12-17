3dc90a9631f9421a5aaf3d1aded8bfb9eacd98b5
Kirill Taran <kirill.taran@hotmail.com>
2019-12-17 20:17:53 +0300

    Refactoring

--- HappyTravel.MailSender/Formatters/EnumFormatter.cs
+++ HappyTravel.MailSender/Formatters/EnumFormatter.cs
@@ -3,7 +3,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Runtime.Serialization;
 
-namespace HappyTravel.Edo.Api.Infrastructure.Formatters
+namespace HappyTravel.MailSender.Formatters
 {
     public static class EnumFormatter
     {
