683d4f889b30e2c50683a61459fab9f96a32c30c
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2019-12-25 01:33:09 +0500

    Added configurable edo public url (to use in e-mails)

--- Api/Services/Customers/CustomerInvitationOptions.cs
+++ Api/Services/Customers/CustomerInvitationOptions.cs
@@ -3,5 +3,6 @@ namespace HappyTravel.Edo.Api.Services.Customers
     public class CustomerInvitationOptions
     {
         public string MailTemplateId { get; set; }
+        public string EdoPublicUrl { get; set; }
     }
 }
