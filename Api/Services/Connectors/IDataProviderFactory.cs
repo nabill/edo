c36c1e9a499060dd818cc84ae6db94bc0ab684e9
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-01-27 12:52:06 +0500

    Data providers factory and abstractions

---
+++ Api/Services/Connectors/IDataProviderFactory.cs
@@ -0,0 +1,10 @@
+using System.Collections.Generic;
+using HappyTravel.Edo.Common.Enums;
+
+namespace HappyTravel.Edo.Api.Services.Connectors
+{
+    public interface IDataProviderFactory
+    {
+        IReadOnlyCollection<(DataProviders Key, IDataProvider Provider)> GetAll();
+    }
+}
