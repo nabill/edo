ca292cb7c1597c01098cfc6091a28f8ff79cfc0d
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-01-27 13:10:22 +0500

    Revert "Data providers factory and abstractions"
    This reverts commit c36c1e9a499060dd818cc84ae6db94bc0ab684e9.

--- Api/Infrastructure/Options/DataProviderOptions.cs
+++ Api/Infrastructure/Options/DataProviderOptions.cs
@@ -1,11 +1,7 @@
-﻿using System.Collections.Generic;
-
-namespace HappyTravel.Edo.Api.Infrastructure.Options
+﻿namespace HappyTravel.Edo.Api.Infrastructure.Options
 {
     public class DataProviderOptions
     {
         public string Netstorming { get; set; }
-        public string Illusions { get; set; }
-        public List<Common.Enums.DataProviders> EnabledProviders { get; set; }
     }
 }
