7aaa34bc7a84c623ca2e525dc4165267bee3b9db
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-02-10 23:58:52 +0500

    Authorization policies in ASP .NET Core style #2

---
+++ Api/Filters/Authorization/InCompanyPermissionAuthorizationHandler.cs
@@ -0,0 +1,46 @@
+using System.Threading.Tasks;
+using HappyTravel.Edo.Api.Services.Customers;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.Extensions.Logging;
+
+namespace HappyTravel.Edo.Api.Filters.Authorization
+{
+    public class InCompanyPermissionAuthorizationHandler : AuthorizationHandler<InCompanyPermissionsAuthorizationRequirement>
+    {
+        public InCompanyPermissionAuthorizationHandler(ICustomerContext customerContext,
+            IPermissionChecker permissionChecker,
+            ILogger<InCompanyPermissionAuthorizationHandler> logger)
+        {
+            _customerContext = customerContext;
+            _permissionChecker = permissionChecker;
+            _logger = logger;
+        }
+
+
+        protected override async Task HandleRequirementAsync(AuthorizationHandlerContext context, InCompanyPermissionsAuthorizationRequirement requirement)
+        {
+            var (_, isCustomerFailure, customer, customerError) = await _customerContext.GetCustomerInfo();
+            if (isCustomerFailure)
+            {
+                // TODO: log customer not found
+                context.Fail();
+                return;
+            }
+
+            var (_, isPermissionFailure, permissionError) = await _permissionChecker.CheckInCompanyPermission(customer, requirement.Permissions);
+            if (isPermissionFailure)
+            {
+                // TODO: log permission not found
+                context.Fail();
+                return;
+            }
+
+            context.Succeed(requirement);
+        }
+
+
+        private readonly ICustomerContext _customerContext;
+        private readonly ILogger<InCompanyPermissionAuthorizationHandler> _logger;
+        private readonly IPermissionChecker _permissionChecker;
+    }
+}
