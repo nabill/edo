d4b17d1d0f1ab13d7a4c4828ec4e3aa6b092720a
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-02-27 16:43:25 +0500

    Changed DbSet mocks to use MockQueryable package

--- HappyTravel.Edo.UnitTests/Infrastructure/DbSetMocks/DbSetMockProvider.cs
+++ HappyTravel.Edo.UnitTests/Infrastructure/DbSetMocks/DbSetMockProvider.cs
@@ -1,39 +1,12 @@
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading;
 using Microsoft.EntityFrameworkCore;
-using Moq;
+using MockQueryable.Moq;
 
 namespace HappyTravel.Edo.UnitTests.Infrastructure.DbSetMocks
 {
     public class DbSetMockProvider
     {
-        public static DbSet<T> GetDbSetMock<T>(IEnumerable<T> enumerable) where T : class
-        {
-            var mock = new Mock<DbSet<T>>();
-            var queryable = enumerable.AsQueryable();
-
-            mock.As<IAsyncEnumerable<T>>()
-                .Setup(m => m.GetAsyncEnumerator(CancellationToken.None))
-                .Returns(new TestAsyncEnumerator<T>(queryable.GetEnumerator()));
-
-            mock.As<IQueryable<T>>()
-                .Setup(m => m.Provider)
-                .Returns(new TestAsyncQueryProvider<T>(queryable.Provider));
-
-            mock.As<IQueryable<T>>()
-                .Setup(m => m.Expression)
-                .Returns(queryable.Expression);
-
-            mock.As<IQueryable<T>>()
-                .Setup(m => m.ElementType)
-                .Returns(queryable.ElementType);
-
-            mock.As<IQueryable<T>>()
-                .Setup(m => m.GetEnumerator())
-                .Returns(() => queryable.GetEnumerator());
-
-            return mock.Object;
-        }
+        public static DbSet<T> GetDbSetMock<T>(IEnumerable<T> enumerable) where T : class => enumerable.AsQueryable().BuildMockDbSet().Object;
     }
 }
