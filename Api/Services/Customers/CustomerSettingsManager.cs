dc3bfb847cc2b85a6c0a2a7d13aa93ae460491bf
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-03-13 14:55:59 +0500

    Getting rid of error while getting customer settings + simplify code

--- Api/Services/Customers/CustomerSettingsManager.cs
+++ Api/Services/Customers/CustomerSettingsManager.cs
@@ -1,6 +1,5 @@
 using System.Linq;
 using System.Threading.Tasks;
-using CSharpFunctionalExtensions;
 using HappyTravel.Edo.Api.Infrastructure.Converters;
 using HappyTravel.Edo.Api.Models.Customers;
 using HappyTravel.Edo.Data;
@@ -18,46 +17,34 @@ namespace HappyTravel.Edo.Api.Services.Customers
         }
 
 
-        public async Task<Result> SetAppSettings(CustomerInfo customerInfo, string appSettings)
+        public async Task SetAppSettings(CustomerInfo customerInfo, string appSettings)
         {
-            var (_, isFailure, customer, error) = await GetCustomer(customerInfo);
-            if (isFailure)
-                return Result.Fail(error);
-
+            var customer = await GetCustomer(customerInfo);
             customer.AppSettings = appSettings;
             _context.Update(customer);
             await _context.SaveChangesAsync();
-            return Result.Ok();
         }
 
 
-        public async Task<Result<string>> GetAppSettings(CustomerInfo customerInfo)
+        public async Task<string> GetAppSettings(CustomerInfo customerInfo)
         {
-            var settings = await _context.Customers
+            return await _context.Customers
                 .Where(c => c.Id == customerInfo.CustomerId)
                 .Select(c => c.AppSettings)
-                .SingleOrDefaultAsync();
-
-            return settings == default
-                ? Result.Fail<string>("Could not find the customer")
-                : Result.Ok(settings);
+                .SingleOrDefaultAsync() ?? string.Empty;
         }
 
 
-        public async Task<Result> SetUserSettings(CustomerInfo customerInfo, CustomerUserSettings userSettings)
+        public async Task SetUserSettings(CustomerInfo customerInfo, CustomerUserSettings userSettings)
         {
-            var (_, isFailure, customer, error) = await GetCustomer(customerInfo);
-            if (isFailure)
-                return Result.Fail(error);
-
+            var customer = await GetCustomer(customerInfo);
             customer.UserSettings = _serializer.SerializeObject(userSettings);
             _context.Update(customer);
             await _context.SaveChangesAsync();
-            return Result.Ok();
         }
 
 
-        public async Task<Result<CustomerUserSettings>> GetUserSettings(CustomerInfo customerInfo)
+        public async Task<CustomerUserSettings> GetUserSettings(CustomerInfo customerInfo)
         {
             var settings = await _context.Customers
                 .Where(c => c.Id == customerInfo.CustomerId)
@@ -65,20 +52,13 @@ namespace HappyTravel.Edo.Api.Services.Customers
                 .SingleOrDefaultAsync();
 
             return settings == default
-                ? Result.Fail<CustomerUserSettings>("Could not find the customer")
-                : Result.Ok(_serializer.DeserializeObject<CustomerUserSettings>(settings));
+                ? default
+                : _serializer.DeserializeObject<CustomerUserSettings>(settings);
         }
 
 
-        private async Task<Result<Customer>> GetCustomer(CustomerInfo customerInfo)
-        {
-            var customer = await _context.Customers
-                .SingleOrDefaultAsync(c => c.Id == customerInfo.CustomerId);
-
-            return customer == default
-                ? Result.Fail<Customer>("Could not find the customer")
-                : Result.Ok(customer);
-        }
+        private Task<Customer> GetCustomer(CustomerInfo customerInfo) => _context.Customers
+            .SingleOrDefaultAsync(c => c.Id == customerInfo.CustomerId);
 
 
         private readonly EdoContext _context;
--- Api/Services/Customers/ICustomerSettingsManager.cs
+++ Api/Services/Customers/ICustomerSettingsManager.cs
@@ -1,17 +1,16 @@
 using System.Threading.Tasks;
-using CSharpFunctionalExtensions;
 using HappyTravel.Edo.Api.Models.Customers;
 
 namespace HappyTravel.Edo.Api.Services.Customers
 {
     public interface ICustomerSettingsManager
     {
-        Task<Result> SetAppSettings(CustomerInfo customerInfo, string appSettings);
+        Task SetAppSettings(CustomerInfo customerInfo, string appSettings);
 
-        Task<Result<string>> GetAppSettings(CustomerInfo customerInfo);
+        Task<string> GetAppSettings(CustomerInfo customerInfo);
 
-        Task<Result> SetUserSettings(CustomerInfo customerInfo, CustomerUserSettings userSettings);
+        Task SetUserSettings(CustomerInfo customerInfo, CustomerUserSettings userSettings);
 
-        Task<Result<CustomerUserSettings>> GetUserSettings(CustomerInfo customerInfo);
+        Task<CustomerUserSettings> GetUserSettings(CustomerInfo customerInfo);
     }
 }
