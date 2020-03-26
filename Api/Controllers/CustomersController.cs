8bc65f51a68124abe8535374f3701162b6375658
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2020-03-26 17:19:19 +0500

    Endpoint to get all possible permissions

--- Api/Controllers/CustomersController.cs
+++ Api/Controllers/CustomersController.cs
@@ -3,6 +3,7 @@ using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Threading.Tasks;
+using HappyTravel.Edo.Api.Extensions;
 using HappyTravel.Edo.Api.Filters.Authorization.CompanyStatesFilters;
 using HappyTravel.Edo.Api.Filters.Authorization.CustomerExistingFilters;
 using HappyTravel.Edo.Api.Filters.Authorization.InCompanyPermissionFilters;
@@ -347,6 +348,16 @@ namespace HappyTravel.Edo.Api.Controllers
             return Ok(settings);
         }
 
+
+        /// <summary>
+        ///     Gets all possible permissions
+        /// </summary>
+        /// <returns> Array of all permission names </returns>
+        [HttpGet("all-permissions-list")]
+        [ProducesResponseType(typeof(IEnumerable<InCompanyPermissions>), (int)HttpStatusCode.OK)]
+        [MinCompanyState(CompanyStates.ReadOnly)]
+        public IActionResult GetAllPermissionsList() => Ok(InCompanyPermissions.All.ToList().Where(p => p != InCompanyPermissions.All));
+        
 
         private async Task<string> GetUserEmail()
         {
