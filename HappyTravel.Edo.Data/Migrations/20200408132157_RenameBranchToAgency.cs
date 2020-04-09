ba0c5c0d5c0bb9d035d5e17ccedf9f9d4806155e
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2020-04-09 15:39:56 +0500

    Branch to Agency migration

---
+++ HappyTravel.Edo.Data/Migrations/20200408132157_RenameBranchToAgency.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class RenameBranchToAgency : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Branches
+            migrationBuilder.RenameIndex(
+                table: "Branches",
+                name: "IX_Branches_CounterpartyId",
+                newName: "IX_Agencies_CounterpartyId");
+
+            migrationBuilder.RenameIndex(
+                table: "Branches",
+                name: "PK_Branches",
+                newName: "PK_Agencies");
+
+            migrationBuilder.RenameTable(
+                name: "Branches",
+                newName: "Agencies");
+
+            // CustomerCompanyRelations
+            migrationBuilder.RenameColumn(
+                table: "CustomerCounterpartyRelations",
+                name: "BranchId",
+                newName: "AgencyId");
+
+            // MarkupPolicies
+            migrationBuilder.RenameColumn(
+                table: "MarkupPolicies",
+                name: "BranchId",
+                newName: "AgencyId");
+
+            migrationBuilder.RenameIndex(
+                table: "MarkupPolicies",
+                name: "IX_MarkupPolicies_BranchId",
+                newName: "IX_MarkupPolicies_AgencyId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            // Branches
+
+            migrationBuilder.RenameTable(
+                name: "Agencies",
+                newName: "Branches");
+
+            migrationBuilder.RenameIndex(
+                table: "Branches",
+                name: "IX_Agencies_CounterpartyId",
+                newName: "IX_Branches_CounterpartyId");
+
+            migrationBuilder.RenameIndex(
+                table: "Branches",
+                name: "PK_Agencies",
+                newName: "PK_Branches");
+
+            // CustomerCompanyRelations
+            migrationBuilder.RenameColumn(
+                table: "CustomerCounterpartyRelations",
+                name: "AgencyId",
+                newName: "BranchId");
+
+            // MarkupPolicies
+            migrationBuilder.RenameColumn(
+                table: "MarkupPolicies",
+                name: "AgencyId",
+                newName: "BranchId");
+
+            migrationBuilder.RenameIndex(
+                table: "MarkupPolicies",
+                name: "IX_MarkupPolicies_AgencyId",
+                newName: "IX_MarkupPolicies_BranchId");
+        }
+    }
+}
