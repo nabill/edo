6f8b0092ca7a9b0cc07262b9340ef84d033cd30b
Hasmik <hasmik_harutyunyan@outlook.com>
2020-05-04 14:31:50 +0400

    renamings

--- HappyTravel.Edo.Data/Migrations/20200504092528_RankForPredictionsSearch.cs
+++ HappyTravel.Edo.Data/Migrations/20200504092528_RankForPredictionsSearch.cs
@@ -6,7 +6,7 @@ namespace HappyTravel.Edo.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var alterSearchLocationRankFunction = @"
+            var alterlocationRankFunction = @"
             CREATE OR REPLACE FUNCTION public.get_location_rank(searchtext text, locationtsvector tsvector)
             RETURNS int
             LANGUAGE plpgsql
@@ -18,9 +18,9 @@ namespace HappyTravel.Edo.Data.Migrations
             RETURN location_rank;
 	            END;
             $function$;";
-			
-            migrationBuilder.Sql(alterSearchLocationRankFunction);
-            
+
+            migrationBuilder.Sql(alterlocationRankFunction);
+
             var alterConcatenatedLocationTsVectorFunction = @"
 			CREATE OR REPLACE FUNCTION public.get_location_concatenated_tsvector(name jsonb, locality jsonb, country jsonb)
 				RETURNS tsvector
@@ -41,11 +41,12 @@ namespace HappyTravel.Edo.Data.Migrations
 			$function$;";
 
             migrationBuilder.Sql(alterConcatenatedLocationTsVectorFunction);
-            
-            var createIndexGetLocationConcatenatedTsVector = "CREATE INDEX IF NOT EXISTS \"IDX_GetLocationConcatenatedTsVector\" ON \"Locations\" USING gin(get_location_concatenated_tsvector(\"Locality\", \"Name\", \"Country\"))";
+
+            var createIndexGetLocationConcatenatedTsVector =
+                "CREATE INDEX IF NOT EXISTS \"IDX_GetLocationConcatenatedTsVector\" ON \"Locations\" USING gin(get_location_concatenated_tsvector(\"Locality\", \"Name\", \"Country\"))";
             migrationBuilder.Sql(createIndexGetLocationConcatenatedTsVector);
 
-            var alterLocationtsvectorFunction = @"
+            var alterLocationTsVectorFunction = @"
 			CREATE OR REPLACE FUNCTION public.get_location_tsvector(name jsonb, locality jsonb, country jsonb)
 			RETURNS tsvector
 			LANGUAGE plpgsql
@@ -60,27 +61,29 @@ namespace HappyTravel.Edo.Data.Migrations
 							 END IF;
 							 RETURN setweight(name_vector, 'A') || setweight(locality_vector,'B') || setweight(country_vector,'C');
 							 END $function$;";
-           
-            migrationBuilder.Sql(alterLocationtsvectorFunction);
-            
+
+            migrationBuilder.Sql(alterLocationTsVectorFunction);
+
             var reindexSql = "REINDEX INDEX \"IDX_GetLocationTsVector\"";
             migrationBuilder.Sql(reindexSql);
-            
-            var alterSearchLocationsFunction = "CREATE OR REPLACE FUNCTION public.search_locations(search_text text, location_type integer, take_limit integer DEFAULT 10) \n RETURNS SETOF \"Locations\" LANGUAGE plpgsql AS $function$ \n DECLARE parsed_ts_query varchar := plainto_tsquery(search_text) :: varchar; \n search_ts_query tsquery;\n BEGIN -- If parsed query is empty, we cannot execute fulltext search  \n IF parsed_ts_query <> '' THEN search_ts_query := to_tsquery(parsed_ts_query || ':*');\n RETURN QUERY\n SELECT loc.*\n FROM public.\"Locations\" as loc\n WHERE get_location_tsvector(\"Name\", \"Locality\", \"Country\") @@ search_ts_query\n   AND \"Type\" = location_type\n ORDER BY get_location_rank(search_text, get_location_concatenated_tsvector(\"Name\", \"Locality\", \"Country\")) DESC,\nts_rank( get_location_tsvector(\"Name\", \"Locality\", \"Country\"), search_ts_query) DESC\n LIMIT (take_limit);\n ELSE RETURN QUERY\n SELECT *\n FROM public.\"Locations\"\n LIMIT 0;\n END IF;\n END;\n $function$;";
+
+            var alterSearchLocationsFunction =
+                "CREATE OR REPLACE FUNCTION public.search_locations(search_text text, location_type integer, take_limit integer DEFAULT 10) \n RETURNS SETOF \"Locations\" LANGUAGE plpgsql AS $function$ \n DECLARE parsed_ts_query varchar := plainto_tsquery(search_text) :: varchar; \n search_ts_query tsquery;\n BEGIN -- If parsed query is empty, we cannot execute fulltext search  \n IF parsed_ts_query <> '' THEN search_ts_query := to_tsquery(parsed_ts_query || ':*');\n RETURN QUERY\n SELECT loc.*\n FROM public.\"Locations\" as loc\n WHERE get_location_tsvector(\"Name\", \"Locality\", \"Country\") @@ search_ts_query\n   AND \"Type\" = location_type\n ORDER BY get_location_rank(search_text, get_location_concatenated_tsvector(\"Name\", \"Locality\", \"Country\")) DESC,\nts_rank( get_location_tsvector(\"Name\", \"Locality\", \"Country\"), search_ts_query) DESC\n LIMIT (take_limit);\n ELSE RETURN QUERY\n SELECT *\n FROM public.\"Locations\"\n LIMIT 0;\n END IF;\n END;\n $function$;";
             migrationBuilder.Sql(alterSearchLocationsFunction);
-            
         }
+
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-	        migrationBuilder.Sql("DROP INDEX \"IDX_GetLocationConcatenatedTsVector\";");
-	        migrationBuilder.Sql("DROP FUNCTION get_location_concatenated_tsvector;");
-	        migrationBuilder.Sql("DROP FUNCTION get_location_rank;");
-	        
-	        var alterSearchLocationsFunction = "CREATE OR REPLACE FUNCTION public.search_locations(search_text text, location_type integer, take_limit integer DEFAULT 10) \n RETURNS SETOF \"Locations\" LANGUAGE plpgsql AS $function$ \n DECLARE parsed_ts_query varchar := plainto_tsquery(search_text) :: varchar; \n search_ts_query tsquery;\n BEGIN -- If parsed query is empty, we cannot execute fulltext search  \n IF parsed_ts_query <> '' THEN search_ts_query := to_tsquery(parsed_ts_query || ':*');\n RETURN QUERY\n SELECT loc.*\n FROM public.\"Locations\" as loc\n WHERE get_location_tsvector(\"Name\", \"Locality\", \"Country\") @@ search_ts_query\n   AND \"Type\" = location_type\n ORDER BY ts_rank( get_location_tsvector(\"Name\", \"Locality\", \"Country\"), search_ts_query) DESC\n LIMIT (take_limit);\n ELSE RETURN QUERY\n SELECT *\n FROM public.\"Locations\"\n LIMIT 0;\n END IF;\n END;\n $function$;";
-	        migrationBuilder.Sql(alterSearchLocationsFunction);
-	        
-	        var alterGetLocationsTsVector = @"CREATE
+            migrationBuilder.Sql("DROP INDEX \"IDX_GetLocationConcatenatedTsVector\";");
+            migrationBuilder.Sql("DROP FUNCTION get_location_concatenated_tsvector;");
+            migrationBuilder.Sql("DROP FUNCTION get_location_rank;");
+
+            var alterSearchLocationsFunction =
+                "CREATE OR REPLACE FUNCTION public.search_locations(search_text text, location_type integer, take_limit integer DEFAULT 10) \n RETURNS SETOF \"Locations\" LANGUAGE plpgsql AS $function$ \n DECLARE parsed_ts_query varchar := plainto_tsquery(search_text) :: varchar; \n search_ts_query tsquery;\n BEGIN -- If parsed query is empty, we cannot execute fulltext search  \n IF parsed_ts_query <> '' THEN search_ts_query := to_tsquery(parsed_ts_query || ':*');\n RETURN QUERY\n SELECT loc.*\n FROM public.\"Locations\" as loc\n WHERE get_location_tsvector(\"Name\", \"Locality\", \"Country\") @@ search_ts_query\n   AND \"Type\" = location_type\n ORDER BY ts_rank( get_location_tsvector(\"Name\", \"Locality\", \"Country\"), search_ts_query) DESC\n LIMIT (take_limit);\n ELSE RETURN QUERY\n SELECT *\n FROM public.\"Locations\"\n LIMIT 0;\n END IF;\n END;\n $function$;";
+            migrationBuilder.Sql(alterSearchLocationsFunction);
+
+            var alterGetLocationTsVector = @"CREATE
                     OR REPLACE FUNCTION public.get_location_tsvector(name jsonb, locality jsonb, country jsonb) RETURNS tsvector LANGUAGE plpgsql IMMUTABLE AS $function$ DECLARE country_vector tsvector;
                     locality_vector tsvector;
                     name_vector tsvector;
@@ -94,10 +97,10 @@ namespace HappyTravel.Edo.Data.Migrations
                     RETURN setweight(name_vector, 'A') || setweight(locality_vector, 'B') || setweight(country_vector, 'C');
                     END $function$;";
 
-	        migrationBuilder.Sql(alterGetLocationsTsVector);
+            migrationBuilder.Sql(alterGetLocationTsVector);
 
-	        var reindexSql = "REINDEX INDEX \"IDX_GetLocationTsVector\"";
-	        migrationBuilder.Sql(reindexSql);
+            var reindexSql = "REINDEX INDEX \"IDX_GetLocationTsVector\"";
+            migrationBuilder.Sql(reindexSql);
         }
     }
 }
