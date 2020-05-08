dbf79ecaba57e4631aef3b60f00521ee812b2bb9
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-05-08 13:48:44 +0500

    Remove ServiceDetails column in booking - migration

---
+++ HappyTravel.Edo.Data/Migrations/20200508083249_BookingRemoveServiceDetails.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class BookingRemoveServiceDetails : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ServiceDetails",
+                table: "Bookings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "ServiceDetails",
+                table: "Bookings",
+                type: "jsonb",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
