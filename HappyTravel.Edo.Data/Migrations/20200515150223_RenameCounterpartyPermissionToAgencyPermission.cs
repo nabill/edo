8a5e70c024310412aa67ebed7cee2627616c7602
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2020-05-15 20:20:28 +0500

    Migration with renamings

---
+++ HappyTravel.Edo.Data/Migrations/20200515150223_RenameCounterpartyPermissionToAgencyPermission.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class RenameCounterpartyPermissionToAgencyPermission : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                table: "AgentCounterpartyRelations",
+                name: "InCounterpartyPermissions",
+                newName: "InAgencyPermissions");
+
+            migrationBuilder.RenameTable(
+                name: "AgentCounterpartyRelations",
+                newName: "AgentAgencyRelations");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameTable(
+                name: "AgentAgencyRelations",
+                newName: "AgentCounterpartyRelations");
+
+            migrationBuilder.RenameColumn(
+                table: "AgentCounterpartyRelations",
+                newName: "InAgencyPermissions",
+                name: "InCounterpartyPermissions");
+        }
+    }
+}
