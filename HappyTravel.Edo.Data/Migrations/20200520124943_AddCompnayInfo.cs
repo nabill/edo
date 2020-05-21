4ef97adc5b510eba3d27a2b14ad869bc9ea93683
Hasmik <hasmik_harutyunyan@outlook.com>
2020-05-21 12:28:28 +0400

    fixed company phone number

--- HappyTravel.Edo.Data/Migrations/20200520124943_AddCompnayInfo.cs
+++ HappyTravel.Edo.Data/Migrations/20200520124943_AddCompnayInfo.cs
@@ -7,7 +7,7 @@ namespace HappyTravel.Edo.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             var sql =
-                "INSERT INTO public.\"StaticData\" (\"Type\", \"Data\") VALUES (1, '{\"Name\":\"HappyTravelDotCom Travel and Tourism LLC\",\"Address\":\"B105, Saraya Avenue building,Garhoud, Deira\",\"Country\":\"United Arab Emirates\",\"City\":\"Dubai\",\"Phone\":\"+971-4- 2940007\",\"Email\":\"info@happytravel.com\",\"PostalCode\":\"Box 36690\",\"TRN\":\"100497287100003\",\"IATA\":\"96-0 4653\",\"TradeLicense\":\"828719\"}')";
+                "INSERT INTO public.\"StaticData\" (\"Type\", \"Data\") VALUES (1, '{\"Name\":\"HappyTravelDotCom Travel and Tourism LLC\",\"Address\":\"B105, Saraya Avenue building,Garhoud, Deira\",\"Country\":\"United Arab Emirates\",\"City\":\"Dubai\",\"Phone\":\"+971-4-2940007\",\"Email\":\"info@happytravel.com\",\"PostalCode\":\"Box 36690\",\"TRN\":\"100497287100003\",\"IATA\":\"96-0 4653\",\"TradeLicense\":\"828719\"}')";
             migrationBuilder.Sql(sql);
         }
 
