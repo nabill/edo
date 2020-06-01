6b77930bfe5b4a597ec20db6bd8fb7e3dbcf9344
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-06-01 19:32:03 +0500

    Replaced stub with moq implementation

--- HappyTravel.Edo.UnitTests/Availability/Step1/Storage/AvailabilityStorageUtils.cs
+++ HappyTravel.Edo.UnitTests/Availability/Step1/Storage/AvailabilityStorageUtils.cs
@@ -1,6 +1,10 @@
+using System;
+using System.Threading;
+using System.Threading.Tasks;
 using FloxDc.CacheFlow;
 using HappyTravel.Edo.Api.Infrastructure.Options;
 using HappyTravel.Edo.Api.Services.Accommodations.Availability;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Options;
 using Moq;
 
@@ -10,7 +14,30 @@ namespace HappyTravel.Edo.UnitTests.Availability.Step1.Storage
     {
         public static IAvailabilityStorage CreateEmptyStorage(IOptions<DataProviderOptions> providerOptions)
         {
-            return new AvailabilityStorage(new InMemoryDistributedFlow(),
+            var memoryFlow = new MemoryFlow(new MemoryCache(Options.Create(new MemoryCacheOptions())));
+            var distributedFlowMock = new Mock<IDistributedFlow>();
+
+            distributedFlowMock
+                .Setup(f => f.Options)
+                .Returns(new FlowOptions());
+
+            distributedFlowMock
+                .Setup(f => f.SetAsync(It.IsAny<string>(), It.IsAny<AvailabilitySearchState>(), It.IsAny<TimeSpan>(), It.IsAny<CancellationToken>()))
+                .Callback<string, AvailabilitySearchState, TimeSpan, CancellationToken>((key, value, timeSpan, _) =>
+                {
+                    memoryFlow.Set(key, value, timeSpan);
+                });
+
+            distributedFlowMock
+                .Setup(f => f.GetAsync<AvailabilitySearchState>(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .Returns<string, CancellationToken>((key, _) =>
+                {
+                    memoryFlow.TryGetValue<AvailabilitySearchState>(key, out var value);
+                    return Task.FromResult(value);
+                });
+                    
+            
+            return new AvailabilityStorage(distributedFlowMock.Object,
                 Mock.Of<IMemoryFlow>(), 
                 providerOptions);
         }
