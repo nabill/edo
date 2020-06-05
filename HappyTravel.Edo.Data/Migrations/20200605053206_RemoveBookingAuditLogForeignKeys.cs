a068675070ec5308b62eea51868553b8d2408fe4
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-06-05 10:42:08 +0500

    Removing foreign indexes from BookingAuditLog

---
+++ HappyTravel.Edo.Data/Migrations/20200605053206_RemoveBookingAuditLogForeignKeys.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class RemoveBookingAuditLogForeignKeys : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            try
+            {
+                migrationBuilder.DropForeignKey(
+                    name: "FK_BookingAuditLog_Bookings_BookingId",
+                    table: "BookingAuditLog");
+            }
+            catch (Exception ex)
+            {
+                // Nothing to do there, FK may exist
+                Console.WriteLine(ex);
+            }
+            
+            migrationBuilder.DropForeignKey(
+                name: "FK_BookingAuditLog_Customers_CustomerId",
+                table: "BookingAuditLog");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
