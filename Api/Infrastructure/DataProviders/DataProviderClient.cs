b3055e37eda0a14addc164175fa8c8baba248db1
Hasmik <hasmik_harutyunyan@outlook.com>
2020-06-10 11:55:13 +0400

    added bearer token to connector requests

--- Api/Infrastructure/DataProviders/DataProviderClient.cs
+++ Api/Infrastructure/DataProviders/DataProviderClient.cs
@@ -5,19 +5,23 @@ using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
-using HappyTravel.Edo.Api.Infrastructure.Http.Extensions;
+using HappyTravel.Edo.Api.Infrastructure.Constants;
+using IdentityModel.Client;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
 using Newtonsoft.Json;
 
 namespace HappyTravel.Edo.Api.Infrastructure.DataProviders
 {
     public class DataProviderClient : IDataProviderClient
     {
-        public DataProviderClient(IHttpClientFactory clientFactory, ILogger<DataProviderClient> logger, IHttpContextAccessor httpContextAccessor)
+        public DataProviderClient(IHttpClientFactory clientFactory, IOptions<ClientCredentialsTokenRequest> tokenRequestOptions,
+            ILogger<DataProviderClient> logger, IHttpContextAccessor httpContextAccessor)
         {
             _clientFactory = clientFactory;
+            _tokenRequest = tokenRequestOptions.Value;
             _logger = logger;
             _httpContextAccessor = httpContextAccessor;
             _serializer = new JsonSerializer();
@@ -40,7 +44,7 @@ namespace HappyTravel.Edo.Api.Infrastructure.DataProviders
         public Task<Result<TOut, ProblemDetails>> Post<TOut>(Uri url, string languageCode = LocalizationHelper.DefaultLanguageCode,
             CancellationToken cancellationToken = default)
             => Send<TOut>(new HttpRequestMessage(HttpMethod.Post, url), languageCode, cancellationToken);
-        
+
 
         public Task<Result<VoidObject, ProblemDetails>> Post(Uri uri, string languageCode = LocalizationHelper.DefaultLanguageCode,
             CancellationToken cancellationToken = default)
@@ -53,28 +57,34 @@ namespace HappyTravel.Edo.Api.Infrastructure.DataProviders
             {
                 Content = new StreamContent(stream)
             }, languageCode, cancellationToken);
-        
-      
+
+
         private static StringContent BuildContent<T>(T requestContent)
             => requestContent is VoidObject
                 ? null
                 : new StringContent(JsonConvert.SerializeObject(requestContent), Encoding.UTF8, "application/json");
-        
-            
-        
-        public async Task<Result<TResponse, ProblemDetails>> Send<TResponse>(HttpRequestMessage request, string languageCode, CancellationToken cancellationToken)
+
+
+        public async Task<Result<TResponse, ProblemDetails>> Send<TResponse>(HttpRequestMessage request, string languageCode,
+            CancellationToken cancellationToken)
         {
             try
             {
                 using var client = _clientFactory.CreateClient();
-                
-                client.DefaultRequestHeaders.Add("Accept-Language", languageCode);
 
+                client.DefaultRequestHeaders.Add("Accept-Language", languageCode);
+                var (_, isFailure, token, getTokenError) = await GetToken();
+                if (isFailure)
+                {
+                    return Result.Failure<TResponse, ProblemDetails>(getTokenError);
+                }
+
+                request.SetBearerToken(token);
                 using var response = await client.SendAsync(request, cancellationToken);
                 await using var stream = await response.Content.ReadAsStreamAsync();
                 using var streamReader = new StreamReader(stream);
                 using var jsonTextReader = new JsonTextReader(streamReader);
-                
+
                 if (!response.IsSuccessStatusCode)
                 {
                     var error = _serializer.Deserialize<ProblemDetails>(jsonTextReader) ??
@@ -96,9 +106,34 @@ namespace HappyTravel.Edo.Api.Infrastructure.DataProviders
         }
 
 
+        private async Task<Result<string, ProblemDetails>> GetToken()
+        {
+            await TokenSemaphore.WaitAsync();
+            var now = DateTime.UtcNow;
+            // We need to cache token because users can send several requests in short periods.
+            if (!_tokenInfo.Equals(default) && _tokenInfo.ExpiryDate >= now)
+                return _tokenInfo.Token;
+
+            using var client = _clientFactory.CreateClient(HttpClientNames.Identity);
+           
+            var tokenResponse = await client.RequestClientCredentialsTokenAsync(_tokenRequest);
+            if (tokenResponse.IsError)
+                Result.Failure<string, ProblemDetails>(
+                    ProblemDetailsBuilder.Build($"Something went wrong while requesting the access token. Error: {tokenResponse.Error}"));
+
+            _tokenInfo = (tokenResponse.AccessToken, now.AddSeconds(tokenResponse.ExpiresIn));
+
+            TokenSemaphore.Release();
+            return Result.Ok<string, ProblemDetails>(_tokenInfo.Token);
+        }
+
+
+        private static (string Token, DateTime ExpiryDate) _tokenInfo;
         private readonly IHttpContextAccessor _httpContextAccessor;
         private readonly IHttpClientFactory _clientFactory;
+        private readonly ClientCredentialsTokenRequest _tokenRequest;
         private readonly JsonSerializer _serializer;
         private readonly ILogger<DataProviderClient> _logger;
+        private static readonly SemaphoreSlim TokenSemaphore = new SemaphoreSlim(1, 1);
     }
 }
