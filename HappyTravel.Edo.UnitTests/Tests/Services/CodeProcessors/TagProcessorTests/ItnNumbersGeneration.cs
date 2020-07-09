250e4882311b899621d3a31e6e52f3b04f7aa2fb
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2020-07-10 02:06:17 +0500

    Add Tests postfix to class namespaces

---
+++ HappyTravel.Edo.UnitTests/Tests/Services/CodeProcessors/TagProcessorTests/ItnNumbersGeneration.cs
@@ -0,0 +1,51 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HappyTravel.Edo.Api.Services.CodeProcessors;
+using HappyTravel.Edo.Data;
+using HappyTravel.Edo.UnitTests.Utility;
+using Moq;
+using NetTopologySuite.Utilities;
+using Xunit;
+
+namespace HappyTravel.Edo.UnitTests.Tests.Services.CodeProcessors.TagProcessorTests
+{
+    public class ItnNumbersGeneration
+    {
+        [Fact]
+        public async Task Generated_itn_numbers_should_be_unique()
+        {
+            var dbContext = CreateDbContextWithDuplicateItnValidation();
+            
+            var tagProcessor = new TagProcessor(dbContext);
+            while (_currentItn < MaxItnNumbersCount)
+            {
+                await tagProcessor.GenerateItn();
+            }
+
+
+            EdoContext CreateDbContextWithDuplicateItnValidation()
+            {
+                var context = MockEdoContextFactory.Create();
+                
+                context.Setup(e => e.GetNextItineraryNumber())
+                    .Returns(() => Task.FromResult(++_currentItn));
+
+                context.Setup(e => e.RegisterItn(It.IsAny<string>()))
+                    .Returns<string>(itn =>
+                    {
+                        if (_registeredItnNumbers.Contains(itn))
+                            throw new AssertionFailedException($"ITN '{itn}' is duplicated after {_registeredItnNumbers.Count} generations");
+
+                        _registeredItnNumbers.Add(itn);
+                        return Task.CompletedTask;
+                    });
+
+                return context.Object;
+            }
+        }
+        
+        private const int MaxItnNumbersCount = 100_000;
+        private long _currentItn = 0L;
+        private readonly HashSet<string> _registeredItnNumbers = new HashSet<string>();
+    }
+}
