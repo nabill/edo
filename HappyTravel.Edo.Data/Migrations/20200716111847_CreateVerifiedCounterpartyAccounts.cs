34c5addb9f63101be1b524301c1c64b5c1135e19
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-07-16 16:21:30 +0500

    Added account creating migrations

---
+++ HappyTravel.Edo.Data/Migrations/20200716111847_CreateVerifiedCounterpartyAccounts.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class CreateVerifiedCounterpartyAccounts : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Accounts for verified counterparties
+            migrationBuilder.Sql(
+                "insert  into \"CounterpartyAccounts\" (\"CounterpartyId\", \"Balance\", \"Currency\", \"Created\")\nselect \"Id\", 0, 1, now()\nfrom \"Counterparties\" counterparty\nwhere counterparty.\"State\" = 1\nand counterparty.\"Id\" not in (select \"CounterpartyId\" from \"CounterpartyAccounts\");");
+
+            // Accounts for agencies in verified counterparties
+            migrationBuilder.Sql(
+                "insert into \"PaymentAccounts\" (\"AgencyId\", \"Balance\", \"CreditLimit\", \"Currency\", \"Created\", \"AuthorizedBalance\")\nselect ag.\"Id\", 0, 0, 1, now(), 0\nfrom \"Agencies\" ag inner join \"Counterparties\" c on ag.\"CounterpartyId\" = c.\"Id\"\nwhere c.\"State\" = 1 and ag.\"Id\" not in (select \"AgencyId\" from \"PaymentAccounts\");");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
