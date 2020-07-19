24b3b65c8559522269707800ea00e83b05901950
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-07-20 01:13:02 +0500

    Added caching for agent permissions

--- Api/Services/Agents/PermissionChecker.cs
+++ Api/Services/Agents/PermissionChecker.cs
@@ -1,6 +1,9 @@
+using System;
 using System.Linq;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
+using FloxDc.CacheFlow;
+using FloxDc.CacheFlow.Extensions;
 using HappyTravel.Edo.Api.Models.Agents;
 using HappyTravel.Edo.Common.Enums;
 using HappyTravel.Edo.Data;
@@ -10,19 +13,24 @@ namespace HappyTravel.Edo.Api.Services.Agents
 {
     public class PermissionChecker : IPermissionChecker
     {
-        public PermissionChecker(EdoContext context)
+        public PermissionChecker(EdoContext context,
+            IDoubleFlow flow)
         {
             _context = context;
+            _flow = flow;
         }
 
 
         public async ValueTask<Result> CheckInAgencyPermission(AgentContext agent, InAgencyPermissions permission)
         {
-            var storedPermissions = await _context.AgentAgencyRelations
-                .Where(r => r.AgentId == agent.AgentId)
-                .Where(r => r.AgencyId == agent.AgencyId)
-                .Select(r => r.InAgencyPermissions)
-                .SingleOrDefaultAsync();
+            var key = _flow.BuildKey(nameof(PermissionChecker), "Permissions", 
+                agent.AgencyId.ToString(),
+                agent.AgentId.ToString());
+            
+            var storedPermissions = await _flow.GetOrSetAsync(
+                key: key, 
+                getValueFunction: async () => await GetPermissions(agent.AgentId, agent.AgencyId), 
+                AgentPermissionsCacheLifeTime);
 
             if (Equals(storedPermissions, default))
                 return Result.Failure("The agent isn't affiliated with the agency");
@@ -30,8 +38,22 @@ namespace HappyTravel.Edo.Api.Services.Agents
             return !storedPermissions.HasFlag(permission)
                 ? Result.Failure($"Agent does not have permission '{permission}'")
                 : Result.Ok();
+
+
+            Task<InAgencyPermissions> GetPermissions(int agentId, int agencyId)
+            {
+                return _context.AgentAgencyRelations
+                    .Where(r => r.AgentId == agentId)
+                    .Where(r => r.AgencyId == agencyId)
+                    .Select(r => r.InAgencyPermissions)
+                    .SingleOrDefaultAsync();
+            }
         }
-        
+
+
+        private static readonly TimeSpan AgentPermissionsCacheLifeTime = TimeSpan.FromMinutes(2);
+
         private readonly EdoContext _context;
+        private readonly IDoubleFlow _flow;
     }
 }
