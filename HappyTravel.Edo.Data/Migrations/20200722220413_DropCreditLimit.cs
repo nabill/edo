83e968bfcca8efc2e558f2648d710e4bb06ea242
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2020-07-23 03:05:21 +0500

    Migration drop credit limit column

---
+++ HappyTravel.Edo.Data/Migrations/20200722220413_DropCreditLimit.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class DropCreditLimit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "CreditLimit",
+                table: "AgencyAccounts");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<decimal>(
+                name: "CreditLimit",
+                table: "AgencyAccounts",
+                type: "numeric",
+                nullable: false,
+                defaultValue: 0m);
+        }
+    }
+}
