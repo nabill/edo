fa7944e3cb016a81eba2b7a7be24fa6266b55efd
Hasmik <hasmik_harutyunyan@outlook.com>
2020-08-28 16:24:45 +0400

    renamed external admin and service worker client names

---
+++ HappyTravel.Edo.Data/Migrations/20200828120915_RenamedServiceWorker.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class RenamedServiceWorker : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"UPDATE public.""ServiceAccounts"" WHERE ""Id"" = 1
+                SET ""ClientId"" ='service_worker'";
+
+            migrationBuilder.Sql(sql);
+        }
+
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = @"UPDATE public.""ServiceAccounts"" WHERE ""Id"" = 1
+                SET ""ClientId"" ='serviceWorker'";
+
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
