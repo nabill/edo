e17dc2ed7450c85a8a70c59900223f00cc26f641
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-09-16 12:12:46 +0500

    Fixed build #3

--- Api/AdministratorServices/IAgentSystemSettingsManagementService.cs
+++ Api/AdministratorServices/IAgentSystemSettingsManagementService.cs
@@ -7,5 +7,7 @@ namespace HappyTravel.Edo.Api.AdministratorServices
     public interface IAgentSystemSettingsManagementService
     {
         Task<Result> SetAvailabilitySearchSettings(int agentId, int agencyId, AgentAvailabilitySearchSettings settings);
+
+        Task<Result<AgentAvailabilitySearchSettings>> GetAvailabilitySearchSettings(int agentId, int agencyId);
     }
 }
