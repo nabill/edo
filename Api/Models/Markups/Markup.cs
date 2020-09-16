60b433a02f9e5f0075b46d319718a5b52678bd1b
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-09-16 13:32:25 +0500

    Disabling markups for agencies if setting is set

--- Api/Models/Markups/Markup.cs
+++ Api/Models/Markups/Markup.cs
@@ -1,6 +1,8 @@
 using System.Collections.Generic;
+using System.Threading.Tasks;
 using HappyTravel.Edo.Api.Services.PriceProcessing;
 using HappyTravel.Edo.Data.Markup;
+using HappyTravel.Money.Enums;
 
 namespace HappyTravel.Edo.Api.Models.Markups
 {
@@ -9,5 +11,11 @@ namespace HappyTravel.Edo.Api.Models.Markups
         public PriceProcessFunction Function { get; set; }
 
         public List<MarkupPolicy> Policies { get; set; }
+        
+        public static Markup Empty => new Markup
+        {
+            Function = (price, currency) =>  new ValueTask<(decimal, Currencies)>((price, currency)) ,
+            Policies = new List<MarkupPolicy>()
+        };
     }
 }
