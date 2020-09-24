e762d5585652aee6eaaa3008ee2181cc94aff718
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2020-09-24 09:51:31 +0500

    Refundable amount extension for Booking

---
+++ Api/Extensions/BookingExtensions.cs
@@ -0,0 +1,15 @@
+﻿using HappyTravel.Edo.Data.Booking;
+using System;
+using System.Linq;
+using HappyTravel.Money.Models;
+
+namespace HappyTravel.Edo.Api.Extensions
+{
+    public static class BookingExtensions
+    {
+        public static MoneyAmount GetRefundableAmount(this Booking booking, DateTime forDate) =>
+            new MoneyAmount(
+                booking.Rooms.Sum(room => room.Price.Amount * (decimal)room.DeadlineDetails.GetRefundableFraction(forDate)),
+                booking.Currency);
+    }
+}
