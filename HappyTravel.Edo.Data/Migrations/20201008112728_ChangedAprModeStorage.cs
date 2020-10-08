93227a540c6e92d48fd955fd4a7d8f32b0f31f26
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-10-08 16:27:56 +0500

    Added a migration for removing Apr column

---
+++ HappyTravel.Edo.Data/Migrations/20201008112728_ChangedAprModeStorage.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class ChangedAprModeStorage : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "AdvancedPurchaseRatesSettings",
+                table: "AgencySystemSettings");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "AdvancedPurchaseRatesSettings",
+                table: "AgencySystemSettings",
+                type: "integer",
+                nullable: true);
+        }
+    }
+}
