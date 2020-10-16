76bfa249ddfacb5864822d9320f4461e1c01fab5
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-10-16 13:38:54 +0500

    Added missing DI to services

--- Api/Infrastructure/ServiceCollectionExtensions.cs
+++ Api/Infrastructure/ServiceCollectionExtensions.cs
@@ -456,6 +456,9 @@ namespace HappyTravel.Edo.Api.Infrastructure
             services.AddTransient<IRoomSelectionService, RoomSelectionService>();
             services.AddTransient<IBookingEvaluationService, BookingEvaluationService>();
             services.AddTransient<IBookingManagementService, BookingManagementService>();
+            services.AddTransient<IBookingRegistrationService, BookingRegistrationService>();
+            services.AddTransient<IBookingChangesProcessor, BookingChangesProcessor>();
+            services.AddTransient<IBookingResponseProcessor, BookingResponseProcessor>();
             services.AddTransient<IBookingsProcessingService, BookingsProcessingService>();
             services.AddTransient<IDeadlineService, DeadlineService>();
 
