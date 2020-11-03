c5bf40bcdcb6ccbfbbdcc9172f2882b481bc61ac
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2020-11-03 16:23:07 +0500

    Added payer and recipient account Ids info into account transfer audit logs

--- Api/Models/Payments/AuditEvents/AccountBalanceLogEventData.cs
+++ Api/Models/Payments/AuditEvents/AccountBalanceLogEventData.cs
@@ -5,14 +5,18 @@ namespace HappyTravel.Edo.Api.Models.Payments.AuditEvents
     public readonly struct AccountBalanceLogEventData
     {
         [JsonConstructor]
-        public AccountBalanceLogEventData(string reason, decimal balance)
+        public AccountBalanceLogEventData(string reason, decimal balance, int? transferPayerAccountId = null, int? transferRecipientAccountId = null)
         {
             Reason = reason;
             Balance = balance;
+            TransferPayerAccountId = transferPayerAccountId;
+            TransferRecipientAccountId = transferRecipientAccountId;
         }
 
 
         public string Reason { get; }
         public decimal Balance { get; }
+        public int? TransferPayerAccountId { get; }
+        public int? TransferRecipientAccountId { get; }
     }
 }
