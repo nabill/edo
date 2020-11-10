bfded346580895b8ec4260c0324b2c5316317143
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2020-11-10 05:46:56 +0500

    Add IsEnabled to relations migration

---
+++ HappyTravel.Edo.Data/Migrations/20201110004609_AddIsEnabledToRelation.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class AddIsEnabledToRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsEnabled",
+                table: "AgentAgencyRelations",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsEnabled",
+                table: "AgentAgencyRelations");
+        }
+    }
+}
