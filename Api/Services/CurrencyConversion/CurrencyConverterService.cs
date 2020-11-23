2a6f66cff29a9a639e2a1e941ae5e836a2d7223f
Kirill Taran <kirill.taran@hotmail.com>
2020-11-24 00:54:29 +0300

    The currency conversion buffer moved to a library

--- Api/Services/CurrencyConversion/CurrencyConverterService.cs
+++ Api/Services/CurrencyConversion/CurrencyConverterService.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
+using HappyTravel.CurrencyConverter;
 using HappyTravel.Edo.Api.Models.Agents;
 using HappyTravel.Edo.Api.Services.PriceProcessing;
 using HappyTravel.Money.Enums;
@@ -11,8 +12,9 @@ namespace HappyTravel.Edo.Api.Services.CurrencyConversion
 {
     public class CurrencyConverterService : ICurrencyConverterService
     {
-        public CurrencyConverterService(ICurrencyRateService rateService)
+        public CurrencyConverterService(ICurrencyRateService rateService, ICurrencyConverterFactory converterFactory)
         {
+            _converterFactory = converterFactory;
             _rateService = rateService;
         }
 
@@ -23,34 +25,34 @@ namespace HappyTravel.Edo.Api.Services.CurrencyConversion
             var currentCurrency = getCurrencyFunc(data);
             if(!currentCurrency.HasValue)
                 return Result.Success(data);
-                
-            if (currentCurrency == Currencies.NotSpecified)
-                return Result.Failure<TData>($"Cannot convert from '{Currencies.NotSpecified}' currency");
             
             if (currentCurrency == TargetCurrency)
                 return Result.Success(data);
+                
+            if (currentCurrency == Currencies.NotSpecified)
+                return Result.Failure<TData>($"Cannot convert from '{Currencies.NotSpecified}' currency");
             
             var (_, isFailure, rate, error) = await _rateService.Get(currentCurrency.Value, TargetCurrency);
             if (isFailure)
                 return Result.Failure<TData>(error);
 
+            var converter = _converterFactory.Create(in rate, currentCurrency.Value, TargetCurrency);
             var convertedDetails = await changePricesFunc(data, price =>
             {
-                var newAmount = price.Amount * rate * (1 + ConversionBuffer);
-                var ceiledAmount = MoneyRounder.Ceil(newAmount, TargetCurrency);
+                var convertedAmount = converter.Convert(price);
+                var ceiledAmount = MoneyRounder.Ceil(convertedAmount);
 
-                return new ValueTask<MoneyAmount>(new MoneyAmount(ceiledAmount, TargetCurrency));
+                return new ValueTask<MoneyAmount>(ceiledAmount);
             });
             
             return Result.Success(convertedDetails);
         }
 
 
-        private const decimal ConversionBuffer = (decimal)0.005;
-        
         // Only USD Currency is supported for now.
         private const Currencies TargetCurrency = Currencies.USD;
 
+        private readonly ICurrencyConverterFactory _converterFactory;
         private readonly ICurrencyRateService _rateService;
     }
 }
