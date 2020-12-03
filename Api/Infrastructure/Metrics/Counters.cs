ccf47556cb0e969d0ea549c9515772ef52e21253
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-12-03 19:16:01 +0500

    Added basic metrics

---
+++ Api/Infrastructure/Metrics/Counters.cs
@@ -0,0 +1,13 @@
+using Prometheus;
+
+namespace HappyTravel.Edo.Api.Infrastructure.Metrics
+{
+    public static class Counters
+    {
+        public static Counter WideAvailabilitySearchTimes = Prometheus.Metrics.CreateCounter("availability_search_counter", "Counts starts of the wide availability search",
+            new CounterConfiguration
+            {
+                LabelNames = new[] {"method", "endpoint"},
+            });
+    }
+}
