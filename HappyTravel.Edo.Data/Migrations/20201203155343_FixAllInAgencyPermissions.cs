9375b249dd13730de3e5efc379b607586d5197d0
Денис Николаев <denis.nikolaev@pedant.ru>
2020-12-03 20:56:55 +0500

    fix "all-permissions-list" endpoint doesnt return first value

---
+++ HappyTravel.Edo.Data/Migrations/20201203155343_FixAllInAgencyPermissions.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FixAllInAgencyPermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"AgentAgencyRelations\" SET \"InAgencyPermissions\" = 2147483647 WHERE \"InAgencyPermissions\" = 2147483646;");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"AgentAgencyRelations\" SET \"InAgencyPermissions\" = 2147483646 WHERE \"InAgencyPermissions\" = 2147483647;");
+        }
+    }
+}
