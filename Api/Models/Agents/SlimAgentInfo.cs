3a097a5f91488614e0264dc1540736ad6b95b6ac
Денис Николаев <denis.nikolaev@pedant.ru>
2020-12-08 16:24:46 +0500

    returned readonly to SlimAgentInfo.cs

--- Api/Models/Agents/SlimAgentInfo.cs
+++ Api/Models/Agents/SlimAgentInfo.cs
@@ -4,7 +4,7 @@ using Newtonsoft.Json;
 
 namespace HappyTravel.Edo.Api.Models.Agents
 {
-    public struct SlimAgentInfo
+    public readonly struct SlimAgentInfo
     {
         [JsonConstructor]
         public SlimAgentInfo(int agentId, string firstName, string lastName, DateTime created, string markupSettings, bool isActive)
@@ -19,26 +19,26 @@ namespace HappyTravel.Edo.Api.Models.Agents
         /// <summary>
         ///     Agent ID.
         /// </summary>
-        public int AgentId { get; set; }
+        public int AgentId { get; init; }
 
         /// <summary>
         ///     First and last name.
         /// </summary>
-        public string Name { get; set; }
+        public string Name { get; init; }
 
         /// <summary>
         /// Created date timestamp.
         /// </summary>
-        public long Created { get; set; }
+        public long Created { get; init; }
 
         /// <summary>
         ///     Markup settings of the agent.
         /// </summary>
-        public string MarkupSettings { get; set; }
+        public string MarkupSettings { get; init; }
 
         /// <summary>
         /// Activity state
         /// </summary>
-        public bool IsActive { get; set; }
+        public bool IsActive { get; init; }
     }
 }
