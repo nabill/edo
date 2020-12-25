5a6ddcb0b6890c0cbf39bb7314fd9de48ba34a05
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-12-25 16:29:15 +0500

    Fixed migration and model

--- HappyTravel.Edo.Data/Migrations/20201225072831_RemoveBookingIdFromPayments.cs
+++ HappyTravel.Edo.Data/Migrations/20201225072831_RemoveBookingIdFromPayments.cs
@@ -7,22 +7,12 @@ namespace HappyTravel.Edo.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.DropIndex(
-                name: "IX_Payments_BookingId",
+                name: "IX_ExternalPayments_BookingId",
                 table: "Payments");
 
             migrationBuilder.DropColumn(
                 name: "BookingId",
                 table: "Payments");
-
-            migrationBuilder.AlterColumn<string>(
-                name: "ReferenceCode",
-                table: "Payments",
-                type: "text",
-                nullable: false,
-                defaultValue: "",
-                oldClrType: typeof(string),
-                oldType: "text",
-                oldNullable: true);
 
             migrationBuilder.CreateIndex(
                 name: "IX_Payments_ReferenceCode",
@@ -36,14 +26,6 @@ namespace HappyTravel.Edo.Data.Migrations
                 name: "IX_Payments_ReferenceCode",
                 table: "Payments");
 
-            migrationBuilder.AlterColumn<string>(
-                name: "ReferenceCode",
-                table: "Payments",
-                type: "text",
-                nullable: true,
-                oldClrType: typeof(string),
-                oldType: "text");
-
             migrationBuilder.AddColumn<int>(
                 name: "BookingId",
                 table: "Payments",
@@ -52,7 +34,7 @@ namespace HappyTravel.Edo.Data.Migrations
                 defaultValue: 0);
 
             migrationBuilder.CreateIndex(
-                name: "IX_Payments_BookingId",
+                name: "IX_ExternalPayments_BookingId",
                 table: "Payments",
                 column: "BookingId");
         }
