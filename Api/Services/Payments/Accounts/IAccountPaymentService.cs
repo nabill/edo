1f7351952018c5389ee35c1e51cd8461441c0900
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2020-12-28 17:28:24 +0500

    Restored and fixed account service payment charge tests

--- Api/Services/Payments/Accounts/IAccountPaymentService.cs
+++ Api/Services/Payments/Accounts/IAccountPaymentService.cs
@@ -13,7 +13,7 @@ namespace HappyTravel.Edo.Api.Services.Payments.Accounts
         Task<bool> CanPayWithAccount(AgentContext agentContext);
         Task<Result<AccountBalanceInfo>> GetAccountBalance(Currencies currency, AgentContext agent);
         Task<Result<AccountBalanceInfo>> GetAccountBalance(Currencies currency, int agencyId);
-        Task<Result<PaymentResponse>> Charge(string referenceCode, MoneyAmount amount, UserInfo user, IPaymentsService paymentsService);
+        Task<Result<PaymentResponse>> Charge(string referenceCode, UserInfo user, IPaymentsService paymentsService);
         Task<Result> Refund(string referenceCode, MoneyAmount refundableAmount, UserInfo user, IPaymentsService paymentsService, string reason);
         Task<Result> TransferToChildAgency(int payerAccountId, int recipientAccountId, MoneyAmount amount, AgentContext agent);
     }
