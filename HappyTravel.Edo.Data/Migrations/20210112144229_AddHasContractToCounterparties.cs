b4a45483f78b681f11a7f3e1e870c25190fd2c2e
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2021-01-12 19:43:28 +0500

    Added a 'HasContract' flag migration

---
+++ HappyTravel.Edo.Data/Migrations/20210112144229_AddHasContractToCounterparties.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class AddHasContractToCounterparties : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "HasContract",
+                table: "Counterparties",
+                type: "boolean",
+                nullable: false,
+                defaultValue: false);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HasContract",
+                table: "Counterparties");
+        }
+    }
+}
