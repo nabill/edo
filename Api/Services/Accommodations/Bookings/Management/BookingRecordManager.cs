fdbaaa95d3894c64da84d646bedb3b413c9f1dbc
Hasmik <hasmik_harutyunyan@outlook.com>
2021-01-26 17:38:42 +0400

    changed detaching method from context

--- Api/Services/Accommodations/Bookings/Management/BookingRecordManager.cs
+++ Api/Services/Accommodations/Bookings/Management/BookingRecordManager.cs
@@ -159,7 +159,7 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Bookings.Management
             booking.Status = status;
             _context.Bookings.Update(booking);
             await _context.SaveChangesAsync();
-            _context.Entry(booking).State = EntityState.Detached;
+            _context.Detach(booking);
         }
 
 
