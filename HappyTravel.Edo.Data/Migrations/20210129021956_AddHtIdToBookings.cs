11a09fd8cd4e909ece62b4b41bc189cf58ca3143
Denis Nikolaev <dfs.str@gmail.com>
2021-01-29 07:27:30 +0500

    added booking creation date and htId to agent booking list api

---
+++ HappyTravel.Edo.Data/Migrations/20210129021956_AddHtIdToBookings.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class AddHtIdToBookings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HtId",
+                table: "Bookings",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HtId",
+                table: "Bookings");
+        }
+    }
+}
