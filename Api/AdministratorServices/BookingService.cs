1bfb205a5fce6d7a70d1b75541c60ca32254d007
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-01-29 21:16:42 +0600

    Added endpoints for receiving list of bookings

---
+++ Api/AdministratorServices/BookingService.cs
@@ -0,0 +1,57 @@
+﻿using CSharpFunctionalExtensions;
+using HappyTravel.Edo.Data;
+using HappyTravel.Edo.Data.Bookings;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HappyTravel.Edo.Api.AdministratorServices
+{
+    public class BookingService : IBookingService
+    {
+        public BookingService(EdoContext context)
+        {
+            _context = context;
+        }
+
+
+        public async Task<Result<List<Booking>>> GetAgencyBookings(int agencyId)
+        {
+            var agency = await _context.Agencies.SingleOrDefaultAsync(agency => agency.Id == agencyId);
+            if (agency is null)
+                return Result.Failure<List<Booking>>($"Agency with ID {agencyId} not found");
+
+            var bookings = await _context.Bookings.Where(booking => booking.AgencyId == agencyId).ToListAsync();
+            
+            return bookings;
+        }
+
+
+        public async Task<Result<List<Booking>>> GetCounterpartyBookings(int counterpartyId)
+        {
+            var counterparty = await _context.Counterparties.SingleOrDefaultAsync(counterparty => counterparty.Id == counterpartyId);
+            if (counterparty is null)
+                return Result.Failure<List<Booking>>($"Counterparty with ID {counterpartyId} not found");
+
+            var bookings = await _context.Bookings.Where(booking => booking.CounterpartyId == counterpartyId).ToListAsync();
+
+            return bookings;
+        }
+
+
+        public async Task<Result<List<Booking>>> GetAgentBookings(int agentId)
+        {
+            var agent = await _context.Agents.SingleOrDefaultAsync(agent => agent.Id == agentId);
+            if (agent is null)
+                return Result.Failure<List<Booking>>($"Agent with ID {agentId} not found");
+
+            var bookings = await _context.Bookings.Where(booking => booking.AgentId == agentId).ToListAsync();
+
+            return bookings;
+        }
+
+
+        private readonly EdoContext _context;
+    }
+}
---
+++ Api/AdministratorServices/IBookingService.cs
@@ -0,0 +1,14 @@
+﻿using CSharpFunctionalExtensions;
+using HappyTravel.Edo.Data.Bookings;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace HappyTravel.Edo.Api.AdministratorServices
+{
+    public interface IBookingService
+    {
+        Task<Result<List<Booking>>> GetAgencyBookings(int agencyId);
+        Task<Result<List<Booking>>> GetCounterpartyBookings(int counterpartyId);
+        Task<Result<List<Booking>>> GetAgentBookings(int agentId);
+    }
+}
