239c41e368ab0445f63ac0822da7c8570fb2e204
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-02-19 20:00:01 +0500

    Move markup formula to relation migration

---
+++ HappyTravel.Edo.Data/Migrations/20210219145917_MoveMarkupFormulaToRelation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class MoveMarkupFormulaToRelation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayedMarkupFormula",
+                table: "Agents");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayedMarkupFormula",
+                table: "AgentAgencyRelations",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "DisplayedMarkupFormula",
+                table: "AgentAgencyRelations");
+
+            migrationBuilder.AddColumn<string>(
+                name: "DisplayedMarkupFormula",
+                table: "Agents",
+                type: "text",
+                nullable: true);
+        }
+    }
+}
