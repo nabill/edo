763d0a68b05a9e4b97726d8d17cfa30240554ce8
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-03-10 11:20:38 +0500

    fixed sentry error EDO-API-8V

---
+++ HappyTravel.Edo.Data/Migrations/20210310061913_ChangeUserInvitationsDataType.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class ChangeUserInvitationsDataType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "UserInvitations",
+                type: "text",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "jsonb",
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "Data",
+                table: "UserInvitations",
+                type: "jsonb",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "text",
+                oldNullable: true);
+        }
+    }
+}
