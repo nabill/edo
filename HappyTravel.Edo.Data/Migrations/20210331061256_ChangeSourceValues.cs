7eda8b16c4f70b05c62706e31df791cb33a5fe3b
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-03-31 12:21:05 +0600

    Added second migration

---
+++ HappyTravel.Edo.Data/Migrations/20210331061256_ChangeSourceValues.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class ChangeSourceValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = "UPDATE \"public\".\"BookingStatusHistory\" SET \"ChangeSource\" = '0' WHERE  \"ChangeSource\" = 2";
+
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            var sql = "UPDATE \"public\".\"BookingStatusHistory\" SET \"ChangeSource\" = '0' WHERE  \"ChangeSource\" = 2";
+
+            migrationBuilder.Sql(sql);
+        }
+    }
+}
