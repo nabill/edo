23af8134a850a972ceaddf94cc388377bf52d9c9
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-04-02 21:32:58 +0600

    Added UserInfo parameter to ProcessResponse method

--- Api/Services/Accommodations/Bookings/ResponseProcessing/IBookingResponseProcessor.cs
+++ Api/Services/Accommodations/Bookings/ResponseProcessing/IBookingResponseProcessor.cs
@@ -1,10 +1,11 @@
 using System.Threading.Tasks;
+using HappyTravel.Edo.Api.Models.Users;
 using HappyTravel.EdoContracts.Accommodations;
 
 namespace HappyTravel.Edo.Api.Services.Accommodations.Bookings.ResponseProcessing
 {
     public interface IBookingResponseProcessor
     {
-        Task ProcessResponse(Booking bookingResponse, Data.Bookings.BookingChangeReason changeReason);
+        Task ProcessResponse(Booking bookingResponse, UserInfo user, Data.Bookings.BookingChangeReason changeReason);
     }
 }
