3ee434888b5a8a727ba27dc232bf6c03d301e13f
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-04-12 14:25:58 +0500

    Review fix

--- HappyTravel.Edo.UnitTests/Tests/Services/Accommodations/Availability/DeadlinePolicyProcessorTests.cs
+++ HappyTravel.Edo.UnitTests/Tests/Services/Accommodations/Availability/DeadlinePolicyProcessorTests.cs
@@ -12,10 +12,11 @@ namespace HappyTravel.Edo.UnitTests.Tests.Services.Accommodations.Availability
     public class DeadlinePolicyProcessorTests
     {
         [Fact]
-        public void Positive_shift_should_be_made_zero()
+        public void Positive_shift_should_be_made_zero_in_deadline_date()
         {
-            var originalDeadline = new Deadline(FromDate);
-            var checkIn = FromDate;
+            var originalDeadlineDate = new DateTime(2021, 1, 15);
+            var originalDeadline = new Deadline(originalDeadlineDate);
+            var checkIn = originalDeadlineDate;
             var shiftSpan = TimeSpan.FromDays(10);
             var settings = new CancellationPolicyProcessSettings { PolicyStartDateShift = shiftSpan };
 
@@ -28,85 +29,98 @@ namespace HappyTravel.Edo.UnitTests.Tests.Services.Accommodations.Availability
         [Fact]
         public void Positive_shift_should_be_made_zero_to_policies()
         {
-
-            var originalDeadline = new Deadline(FromDate, Policies);
-            var checkIn = FromDate;
+            var originalDeadlineDate = new DateTime(2021, 1, 15);
             var shiftSpan = TimeSpan.FromDays(10);
-            var settings = new CancellationPolicyProcessSettings { PolicyStartDateShift = shiftSpan };
-
-            var shiftedDeadline = DeadlinePolicyProcessor.Process(originalDeadline, checkIn, settings);
-            
-            Assert.True(originalDeadline.Policies.Select(p => p.FromDate)
-                .SafeSequenceEqual(shiftedDeadline.Policies.Select(p => p.FromDate)));
-        }
-
-
-        [Fact]
-        public void Shift_should_change_by_exact_amount()
-        {
-            var originalDeadline = new Deadline(FromDate);
-            var checkIn = FromDate;
-            var shiftSpan = TimeSpan.FromDays(-10);
+            var originalDeadline = new Deadline(originalDeadlineDate, Policies);
+            var checkIn = originalDeadlineDate;
             var settings = new CancellationPolicyProcessSettings { PolicyStartDateShift = shiftSpan };
 
             var shiftedDeadline = DeadlinePolicyProcessor.Process(originalDeadline, checkIn, settings);
 
-            Assert.Equal(originalDeadline.Date + shiftSpan, shiftedDeadline.Date);
+            for(int i = 0; i < originalDeadline.Policies.Count; i++)
+                Assert.Equal(originalDeadline.Policies[i].FromDate, shiftedDeadline.Policies[i].FromDate);
         }
 
 
         [Fact]
-        public void Shift_should_change_by_exact_amount_to_policies()
+        public void Shift_should_change_date_by_exact_days_count_in_deadline_date()
         {
-            var originalDeadline = new Deadline(FromDate, Policies);
-            var checkIn = FromDate;
+            var originalDeadlineDate = new DateTime(2021, 1, 15);
+            var expectedShiftedDate = new DateTime(2021, 1, 5);
             var shiftSpan = TimeSpan.FromDays(-10);
+            var originalDeadline = new Deadline(originalDeadlineDate);
+            var checkIn = originalDeadlineDate;
+            var settings = new CancellationPolicyProcessSettings { PolicyStartDateShift = shiftSpan };
+
+            var shiftedDeadline = DeadlinePolicyProcessor.Process(originalDeadline, checkIn, settings);
+
+            Assert.Equal(expectedShiftedDate, shiftedDeadline.Date);
+        }
+
+
+        [Fact]
+        public void Shift_should_change_date_by_exact_days_count_to_policies()
+        {
+            var originalDeadlineDate = new DateTime(2021, 1, 15);
+            var shiftSpan = TimeSpan.FromDays(-10);
+            var expectedShiftedDates = new List<DateTime>
+            {
+                new DateTime(2021, 1, 6),
+                new DateTime(2021, 1, 7),
+                new DateTime(2021, 1, 8),
+                new DateTime(2021, 1, 9),
+            };
+            var originalDeadline = new Deadline(originalDeadlineDate, Policies);
+            var checkIn = originalDeadlineDate;
             var settings = new CancellationPolicyProcessSettings { PolicyStartDateShift = shiftSpan };
 
             var shiftedDeadline = DeadlinePolicyProcessor.Process(originalDeadline, checkIn, settings);
 
             Assert.True(originalDeadline.Policies.Select(p => p.FromDate + shiftSpan)
                 .SafeSequenceEqual(shiftedDeadline.Policies.Select(p => p.FromDate)));
+
+            for (int i = 0; i < originalDeadline.Policies.Count; i++)
+                Assert.Equal(expectedShiftedDates[i], shiftedDeadline.Policies[i].FromDate);
         }
 
 
         [Fact]
         public void Shift_should_keep_percentage_in_policies()
         {
-            var originalDeadline = new Deadline(FromDate, Policies);
-            var checkIn = FromDate;
+            var originalDeadlineDate = new DateTime(2021, 1, 15);
             var shiftSpan = TimeSpan.FromDays(-10);
+            var originalDeadline = new Deadline(originalDeadlineDate, Policies);
+            var checkIn = originalDeadlineDate;
             var settings = new CancellationPolicyProcessSettings { PolicyStartDateShift = shiftSpan };
 
             var shiftedDeadline = DeadlinePolicyProcessor.Process(originalDeadline, checkIn, settings);
 
-            Assert.True(originalDeadline.Policies.Select(p => p.Percentage)
-                .SafeSequenceEqual(shiftedDeadline.Policies.Select(p => p.Percentage)));
+            for (int i = 0; i < originalDeadline.Policies.Count; i++)
+                Assert.Equal(originalDeadline.Policies[i].Percentage, shiftedDeadline.Policies[i].Percentage);
         }
 
 
         [Fact]
         public void Shift_should_use_check_in_if_deadline_null()
         {
-            var originalDeadline = new Deadline(null);
-            var checkIn = FromDate;
+            var originalDeadlineDate = new DateTime(2021, 1, 15);
             var shiftSpan = TimeSpan.FromDays(0);
+            var originalDeadline = new Deadline(null);
+            var checkIn = originalDeadlineDate;
             var settings = new CancellationPolicyProcessSettings { PolicyStartDateShift = shiftSpan };
 
             var shiftedDeadline = DeadlinePolicyProcessor.Process(originalDeadline, checkIn, settings);
 
-            Assert.Equal(FromDate, shiftedDeadline.Date);
+            Assert.Equal(originalDeadlineDate, shiftedDeadline.Date);
         }
 
 
-        private static readonly DateTime FromDate = new DateTime(2021, 1, 1);
-
         private static readonly List<CancellationPolicy> Policies = new List<CancellationPolicy>
         {
-            new CancellationPolicy(FromDate + TimeSpan.FromDays(1), 1d),
-            new CancellationPolicy(FromDate + TimeSpan.FromDays(2), 2d),
-            new CancellationPolicy(FromDate + TimeSpan.FromDays(3), 3d),
-            new CancellationPolicy(FromDate + TimeSpan.FromDays(4), 4d),
+            new CancellationPolicy(new DateTime(2021, 1, 16), 1d),
+            new CancellationPolicy(new DateTime(2021, 1, 17), 2d),
+            new CancellationPolicy(new DateTime(2021, 1, 18), 3d),
+            new CancellationPolicy(new DateTime(2021, 1, 19), 4d),
         };
     }
 }
