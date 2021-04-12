fe1c0460a00ed7a3690ae7e7f8c094bf28ed0239
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-04-12 19:16:00 +0600

    Disabled NotificationCenter calls

--- Api/Services/Accommodations/Bookings/Mailing/BookingDocumentsMailingService.cs
+++ Api/Services/Accommodations/Bookings/Mailing/BookingDocumentsMailingService.cs
@@ -57,11 +57,11 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Bookings.Mailing
 
                     // TODO: We are now sending parameters for mail, but they are not used in NotificationCenter.
                     // Sending by email via NotificationCenter will be implemented in the task AA-128.
-                    await _sendingNotificationsService.Send(agent: new Models.Agents.SlimAgentContext(booking.AgentId, booking.AgencyId), 
+                    /*await _sendingNotificationsService.Send(agent: new Models.Agents.SlimAgentContext(booking.AgentId, booking.AgencyId), 
                         message: JsonSerializer.Serialize(voucherData), 
                         notificationType: NotificationTypes.BookingVoucher, 
                         email: email, 
-                        templateId: _options.VoucherTemplateId);
+                        templateId: _options.VoucherTemplateId);*/
 
                     // TODO: This line will be removed after implementing the task AA-128.
                     return await _mailSender.Send(_options.VoucherTemplateId, email, voucherData);
@@ -111,11 +111,11 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Bookings.Mailing
 
                     // TODO: We are now sending parameters for mail, but they are not used in NotificationCenter.
                     // Sending by email via NotificationCenter will be implemented in the task AA-128.
-                    await _sendingNotificationsService.Send(agent: new Models.Agents.SlimAgentContext(booking.AgentId, booking.AgencyId),
+                    /*await _sendingNotificationsService.Send(agent: new Models.Agents.SlimAgentContext(booking.AgentId, booking.AgencyId),
                         message: JsonSerializer.Serialize(invoiceData),
                         notificationType: NotificationTypes.BookingInvoice,
                         emails: addresses,
-                        templateId: _options.InvoiceTemplateId);
+                        templateId: _options.InvoiceTemplateId);*/
 
                     // TODO: This line will be removed after implementing the task AA-128.
                     return await _mailSender.Send(_options.InvoiceTemplateId, addresses, invoiceData);
