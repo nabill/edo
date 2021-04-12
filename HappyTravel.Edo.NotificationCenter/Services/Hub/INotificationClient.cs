b8a929fb2d5219571904c73e6ac670e36c01869e
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-04-13 03:08:44 +0600

    Removed HappyTravel.Edo.NotificationCenter project

--- HappyTravel.Edo.NotificationCenter/Services/Hub/INotificationClient.cs
+++
@@ -1,9 +0,0 @@
-using System.Threading.Tasks;
-
-namespace HappyTravel.Edo.NotificationCenter.Services.Hub
-{
-    public interface INotificationClient
-    {
-        Task ReceiveMessage(int messageId, string message);
-    }
-}
