7f27ac2a2d72de50eab899ff953b2c79a86de802
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-04-14 16:26:36 +0600

    Added OnConnectedAsync override

--- Api/NotificationCenter/Hubs/NotificationHub.cs
+++ Api/NotificationCenter/Hubs/NotificationHub.cs
@@ -1,16 +1,49 @@
+using HappyTravel.Edo.Api.Infrastructure;
+using HappyTravel.Edo.Data;
 using HappyTravel.Edo.Notifications.Enums;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.SignalR;
+using Microsoft.EntityFrameworkCore;
+using System.Linq;
+using System.Security.Claims;
 using System.Threading.Tasks;
 
 namespace HappyTravel.Edo.Api.NotificationCenter.Hubs
 {
+    [Authorize]
     public class NotificationHub : Hub<INotificationClient>
     {
+        public NotificationHub(EdoContext context)
+        {
+            _context = context;
+        }
+
+
         public static async Task SendPrivateMessage(IHubContext<NotificationHub, INotificationClient> hub, ReceiverTypes receiver, int userId, int messageId, string message)
         {
             await hub.Clients
-                .User(BuildUserId(receiver, userId))
+                .Group(userId.ToString())
+                //.User(BuildUserId(receiver, userId))
                 .ReceiveMessage(messageId, message);
+        }
+
+
+        public override async Task OnConnectedAsync()
+        {
+            var identityId = Context.User?.FindFirstValue("sub");
+            if (string.IsNullOrEmpty(identityId))
+                return;
+
+            var agentId = await _context.Agents
+                .Where(a => a.IdentityHash == HashGenerator.ComputeSha256(identityId))
+                .Select(a => a.Id)
+                .SingleOrDefaultAsync();
+
+            if (agentId == default)
+                return;
+
+            await Groups.AddToGroupAsync(Context.ConnectionId, agentId.ToString());
+            await base.OnConnectedAsync();
         }
 
 
@@ -23,5 +56,8 @@ namespace HappyTravel.Edo.Api.NotificationCenter.Hubs
                 _ => $"unknown-{userId}",
             };
         }
+
+
+        private readonly EdoContext _context;
     }
 }
