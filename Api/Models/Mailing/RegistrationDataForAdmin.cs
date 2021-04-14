9ef6630342b3b4b69e6a82d847d047e2659b1b46
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-04-15 02:27:36 +0500

    Add attribute to serialize enums by name to registration mail info

--- Api/Models/Mailing/RegistrationDataForAdmin.cs
+++ Api/Models/Mailing/RegistrationDataForAdmin.cs
@@ -1,26 +1,41 @@
 using HappyTravel.Edo.Common.Enums;
 using HappyTravel.Money.Enums;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace HappyTravel.Edo.Api.Models.Mailing
 {
     public class RegistrationDataForAdmin : DataWithCompanyInfo
     {
         public CounterpartyRegistrationMailData Counterparty { get; set; }
+
         public string AgentEmail { get; set; }
+
         public string AgentName { get; set; }
 
 
         public struct CounterpartyRegistrationMailData
         {
             public string Name { get; set; }
+
             public string Address { get; set; }
+
+            [JsonConverter(typeof(StringEnumConverter))]
             public PaymentTypes PreferredPaymentMethod { get; set; }
+
             public string Website { get; set; }
+
             public string CountryCode { get; set; }
+
             public string City { get; set; }
+
             public string Phone { get; set; }
+
             public string Fax { get; set; }
+
             public string PostalCode { get; set; }
+
+            [JsonConverter(typeof(StringEnumConverter))]
             public Currencies PreferredCurrency { get; set; }
         }
     }
