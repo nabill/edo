b46992d620191dbd1cbdf2a8212db8cc6c2a01c5
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-04-20 15:23:00 +0600

    Added AccoomodationInfo

---
+++ HappyTravel.Edo.Data/Bookings/AccommodationInfo.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HappyTravel.Edo.Data.Bookings
+{
+    public class AccommodationInfo
+    {
+        public Photo AccommodationPhoto { get; set; }
+    }
+}
