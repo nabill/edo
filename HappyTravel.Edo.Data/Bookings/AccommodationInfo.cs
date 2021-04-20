c1ddc8d019492657b7368b8eb03c2a96afc9df14
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-04-20 18:46:25 +0600

    Added class Photo

--- HappyTravel.Edo.Data/Bookings/AccommodationInfo.cs
+++ HappyTravel.Edo.Data/Bookings/AccommodationInfo.cs
@@ -1,13 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace HappyTravel.Edo.Data.Bookings
+﻿namespace HappyTravel.Edo.Data.Bookings
 {
     public class AccommodationInfo
     {
-        public Photo AccommodationPhoto { get; set; }
+        public Photo Photo { get; set; }
     }
 }
---
+++ HappyTravel.Edo.Data/Bookings/Photo.cs
@@ -0,0 +1,8 @@
+﻿namespace HappyTravel.Edo.Data.Bookings
+{
+    public class Photo
+    {
+        public string Tiltle { get; set; }
+        public string Url { get; set; }
+    }
+}
