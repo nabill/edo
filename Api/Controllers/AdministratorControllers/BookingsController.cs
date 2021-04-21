6b56d8f253cd1387a9b88c5bad97c567eb912c29
Hasmik <hasmik_harutyunyan@outlook.com>
2021-04-21 15:09:33 +0400

    revie changes

--- Api/Controllers/AdministratorControllers/BookingsController.cs
+++ Api/Controllers/AdministratorControllers/BookingsController.cs
@@ -10,6 +10,7 @@ using HappyTravel.Edo.Api.Models.Management.Enums;
 using HappyTravel.Edo.Api.Services.Management;
 using HappyTravel.Edo.Data.Bookings;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Http;
 
 namespace HappyTravel.Edo.Api.Controllers.AdministratorControllers
 {
@@ -27,17 +28,17 @@ namespace HappyTravel.Edo.Api.Controllers.AdministratorControllers
             _bookingService = bookingService;
             _bookingManagementService = bookingManagementService;
         }
-        
-        
+
+
         /// <summary>
         ///     Gets a list of all bookings made by the agency
         /// </summary>
         /// <param name="agencyId">Agency Id</param>
         /// <returns>List of bookings</returns>
         [HttpGet("agencies/{agencyId}/accommodations/bookings")]
-        [ProducesResponseType(typeof(List<Booking>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
-        [AdministratorPermissions(AdministratorPermissions.AgentManagement)]
+        [ProducesResponseType(typeof(List<Booking>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), (int) HttpStatusCode.BadRequest)]
+        [AdministratorPermissions(AdministratorPermissions.BookingManagement)]
         public async Task<IActionResult> GetAgencyBookings([FromRoute] int agencyId)
         {
             var (_, isFailure, bookings, error) = await _bookingService.GetAgencyBookings(agencyId);
@@ -54,9 +55,9 @@ namespace HappyTravel.Edo.Api.Controllers.AdministratorControllers
         /// <param name="counterpartyId">Counterparty Id</param>
         /// <returns>List of bookings</returns>
         [HttpGet("counterparties/{counterpartyId}/accommodations/bookings")]
-        [ProducesResponseType(typeof(List<Booking>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
-        [AdministratorPermissions(AdministratorPermissions.AgentManagement)]
+        [ProducesResponseType(typeof(List<Booking>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), (int) HttpStatusCode.BadRequest)]
+        [AdministratorPermissions(AdministratorPermissions.BookingManagement)]
         public async Task<IActionResult> GetCounterpartyBookings([FromRoute] int counterpartyId)
         {
             var (_, isFailure, bookings, error) = await _bookingService.GetCounterpartyBookings(counterpartyId);
@@ -73,9 +74,9 @@ namespace HappyTravel.Edo.Api.Controllers.AdministratorControllers
         /// <param name="agentId">Agent Id</param>
         /// <returns>List of bookings</returns>
         [HttpGet("agents/{agentId}/accommodations/bookings")]
-        [ProducesResponseType(typeof(List<Booking>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
-        [AdministratorPermissions(AdministratorPermissions.AgentManagement)]
+        [ProducesResponseType(typeof(List<Booking>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), (int) HttpStatusCode.BadRequest)]
+        [AdministratorPermissions(AdministratorPermissions.BookingManagement)]
         public async Task<IActionResult> GetAgentBookings([FromRoute] int agentId)
         {
             var (_, isFailure, bookings, error) = await _bookingService.GetAgentBookings(agentId);
