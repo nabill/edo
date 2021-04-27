e96c5f9fd70deba2561605f92e3d1853a368c6e7
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-04-27 20:01:09 +0500

    Fix

--- Api/Services/Agents/AgentService.cs
+++ Api/Services/Agents/AgentService.cs
@@ -8,6 +8,7 @@ using HappyTravel.Edo.Api.Extensions;
 using HappyTravel.Edo.Api.Infrastructure;
 using HappyTravel.Edo.Api.Models.Agents;
 using HappyTravel.Edo.Api.Models.Users;
+using HappyTravel.Edo.Api.Services.Accommodations.Availability.Steps.BookingEvaluation;
 using HappyTravel.Edo.Common.Enums;
 using HappyTravel.Edo.Data;
 using HappyTravel.Edo.Data.Agents;
@@ -153,7 +154,7 @@ namespace HappyTravel.Edo.Api.Services.Agents
                     cr.Type == AgentAgencyRelationTypes.Master,
                     GetActualPermissions(co.State, cr.InAgencyPermissions),
                     co.State,
-                    co.PreferredPaymentMethod))
+                    BookingPaymentMethodsHelper.GetDefaultPaymentType(co.ContractKind)))
                 .ToListAsync();
         }
 
