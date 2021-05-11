781e83338da8fe09c7c2daaaf8cd1757dd1a9b89
Алексей Шокарев <Alexey.Shokarev@gmail.com>
2021-05-11 13:19:01 +0600

    Added feedback model

---
+++ HappyTravel.Edo.Notifications/Models/FeedbackOnNotification.cs
@@ -0,0 +1,12 @@
+﻿using HappyTravel.Edo.Notifications.Enums;
+using System;
+
+namespace HappyTravel.Edo.Notifications.Models
+{
+    public readonly struct FeedbackOnNotification
+    {
+        public int MessageId { get; init; }
+        public SendingStatuses SendingStatus { get; init; }
+        public DateTime TimeChange { get; init; }
+    }
+}
