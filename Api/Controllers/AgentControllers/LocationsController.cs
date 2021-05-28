5724d8709c101edfb02cdc9893e28b7aeac967e9
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2021-05-28 13:30:57 +0500

    Removed old prediction flow data and logic

--- Api/Controllers/AgentControllers/LocationsController.cs
+++ Api/Controllers/AgentControllers/LocationsController.cs
@@ -1,15 +1,7 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
+﻿using System.Collections.Generic;
 using System.Net;
 using System.Threading.Tasks;
-using CSharpFunctionalExtensions;
-using HappyTravel.Edo.Api.Filters.Authorization.AgentExistingFilters;
-using HappyTravel.Edo.Api.Filters.Authorization.ServiceAccountFilters;
-using HappyTravel.Edo.Api.Infrastructure;
 using HappyTravel.Edo.Api.Models.Locations;
-using HappyTravel.Edo.Api.Services.Agents;
 using HappyTravel.Edo.Api.Services.Locations;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
@@ -22,11 +14,9 @@ namespace HappyTravel.Edo.Api.Controllers.AgentControllers
     [Produces("application/json")]
     public class LocationsController : BaseController
     {
-        public LocationsController(IAgentContextService agentContextService, ILocationService service, ILocationNormalizer locationNormalizer)
+        public LocationsController(ILocationService service)
         {
-            _agentContextService = agentContextService;
             _service = service;
-            _locationNormalizer = locationNormalizer;
         }
 
 
@@ -49,25 +39,10 @@ namespace HappyTravel.Edo.Api.Controllers.AgentControllers
         [AllowAnonymous]
         [HttpGet("regions")]
         [ProducesResponseType(typeof(List<Region>), (int) HttpStatusCode.OK)]
-        public async Task<IActionResult> GetRegions() => Ok(await _service.GetRegions(LanguageCode));
+        public async Task<IActionResult> GetRegions() 
+            => Ok(await _service.GetRegions(LanguageCode));
 
-
-        /// <summary>
-        /// Starts a locations normalization process
-        /// </summary>
-        /// <returns></returns>
-        [ProducesResponseType((int) HttpStatusCode.NoContent)]
-        [HttpPost("normalize")]
-        [ServiceAccountRequired]
-        public async Task<IActionResult> Normalize()
-        {
-            await _locationNormalizer.StartNormalization();
-            return NoContent();
-        }
-
-
-        private readonly IAgentContextService _agentContextService;
+        
         private readonly ILocationService _service;
-        private readonly ILocationNormalizer _locationNormalizer;
     }
 }
--- Api/Services/Locations/ILocationService.cs
+++ Api/Services/Locations/ILocationService.cs
@@ -1,26 +1,13 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
-using CSharpFunctionalExtensions;
-using HappyTravel.Edo.Api.Models.Agents;
-using HappyTravel.Edo.Api.Models.Availabilities;
 using HappyTravel.Edo.Api.Models.Locations;
-using Microsoft.AspNetCore.Mvc;
 
 namespace HappyTravel.Edo.Api.Services.Locations
 {
     public interface ILocationService
     {
-        ValueTask<Result<Location, ProblemDetails>> Get(SearchLocation searchLocation, string languageCode);
-
         Task<List<Country>> GetCountries(string query, string languageCode);
 
-        ValueTask<Result<List<Prediction>, ProblemDetails>> GetPredictions(string query, string session, AgentContext agent, string languageCode);
-
         Task<List<Region>> GetRegions(string languageCode);
-
-        Task Set(IEnumerable<Models.Locations.Location> locations);
-
-        Task<DateTime> GetLastModifiedDate();
     }
 }
