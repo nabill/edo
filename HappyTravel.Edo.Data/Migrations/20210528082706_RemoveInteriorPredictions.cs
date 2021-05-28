9e2cb911036f18e8454126b57dd62ff3228b1fe9
Vadim Mingazhev <vadim.mingazhev@gmail.com>
2021-05-28 13:38:39 +0500

    Fixed drop function sql

--- HappyTravel.Edo.Data/Migrations/20210528082706_RemoveInteriorPredictions.cs
+++ HappyTravel.Edo.Data/Migrations/20210528082706_RemoveInteriorPredictions.cs
@@ -8,7 +8,7 @@ namespace HappyTravel.Edo.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            var removeSearchLocationsFunction = "DROP FUNCTION public.search_locations(search_text text, location_type integer, take_limit integer DEFAULT 10) \n RETURNS SETOF \"Locations\" LANGUAGE plpgsql AS $function$ \n DECLARE parsed_ts_query varchar := plainto_tsquery(replace(search_text, ' ', '')) :: varchar; \n search_ts_query tsquery;\n BEGIN -- If parsed query is empty, we cannot execute fulltext search  \n IF parsed_ts_query <> '' THEN search_ts_query := to_tsquery(parsed_ts_query || ':*');\n RETURN QUERY\n SELECT loc.*\n FROM public.\"Locations\" as loc\n WHERE get_location_tsvector(\"Name\", \"Locality\", \"Country\") @@ search_ts_query\n   AND \"Type\" = location_type\n ORDER BY ts_rank( get_location_tsvector(\"Name\", \"Locality\", \"Country\"), search_ts_query) DESC\n LIMIT (take_limit);\n ELSE RETURN QUERY\n SELECT *\n FROM public.\"Locations\"\n LIMIT 0;\n END IF;\n END;\n $function$;";
+            var removeSearchLocationsFunction = "DROP FUNCTION public.search_locations(search_text text, location_type integer, take_limit integer DEFAULT 10);";
             migrationBuilder.Sql(removeSearchLocationsFunction);
             
             migrationBuilder.DropTable(
