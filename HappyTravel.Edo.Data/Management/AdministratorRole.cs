c2b2d32fb45d299042d1b264a5b2c2d28f34266c
Alexey Shokarev <Alexey.Shokarev@gmail.com>
2021-06-23 13:03:29 +0600

    Added AdministratorRole

---
+++ HappyTravel.Edo.Data/Management/AdministratorRole.cs
@@ -0,0 +1,9 @@
+﻿namespace HappyTravel.Edo.Data.Management
+{
+    public class AdministratorRole
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public AdministratorPermissions Permissions { get; set; }
+    }
+}
