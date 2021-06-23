91483848f47bd1eddcc34afd31b8cdf0b644fafa
Alexey Shokarev <Alexey.Shokarev@gmail.com>
2021-06-23 13:19:44 +0600

    Added string enum converter for enums

--- Api/Models/Management/Enums/ActivityStatus.cs
+++ Api/Models/Management/Enums/ActivityStatus.cs
@@ -1,5 +1,9 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace HappyTravel.Edo.Api.Models.Management.Enums
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum ActivityStatus
     {
         Active,
--- HappyTravel.Edo.Common/Enums/CounterpartyStates.cs
+++ HappyTravel.Edo.Common/Enums/CounterpartyStates.cs
@@ -1,5 +1,9 @@
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
 namespace HappyTravel.Edo.Common.Enums
 {
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum CounterpartyStates
     {
         PendingVerification = 0,
