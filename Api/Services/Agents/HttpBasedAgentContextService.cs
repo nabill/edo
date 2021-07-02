cdcd40131dbd3974d2a37882abee384e1502f648
Konstantin Yefremov <konstyefremov@gmail.com>
2021-07-02 16:53:04 +0500

    Code review fixes

--- Api/Services/Agents/HttpBasedAgentContextService.cs
+++ Api/Services/Agents/HttpBasedAgentContextService.cs
@@ -127,6 +127,7 @@ namespace HappyTravel.Edo.Api.Services.Agents
         {
             // TODO: use counterparty information from headers to get counterparty id
             // TODO: this method assumes that only one relation exists for given AgentId, which is now not true. Needs rework. NIJO-623.
+            // TODO: there are too many requests to database, find a way to get rid of this query
             var inAgencyPermissions = await GetInAgencyPermissionsByIdentityHash(identityHash);
             return await (from agent in _context.Agents
                     from agentAgencyRelation in _context.AgentAgencyRelations.Where(r => r.AgentId == agent.Id)
@@ -148,8 +149,9 @@ namespace HappyTravel.Edo.Api.Services.Agents
         }
         
         
-        private async ValueTask<AgentContext> GetAgentInfoByApiClientCredentials(string name, string passwordHash)
+        private async Task<AgentContext> GetAgentInfoByApiClientCredentials(string name, string passwordHash)
         {
+            // TODO: there are too many requests to database, find a way to get rid of this query
             var inAgencyPermissions = await GetInAgencyPermissionsByApiClientCredentials(name, passwordHash);
             return await (from agent in _context.Agents
                     from agentAgencyRelation in _context.AgentAgencyRelations.Where(r => r.AgentId == agent.Id)
@@ -172,7 +174,7 @@ namespace HappyTravel.Edo.Api.Services.Agents
         }
 
 
-        private async ValueTask<InAgencyPermissions> GetInAgencyPermissionsByIdentityHash(string identityHash)
+        private async Task<InAgencyPermissions> GetInAgencyPermissionsByIdentityHash(string identityHash)
         {
             var agentRoleIds = await (from agent in _context.Agents
                     from agentAgencyRelation in _context.AgentAgencyRelations.Where(r => r.AgentId == agent.Id)
