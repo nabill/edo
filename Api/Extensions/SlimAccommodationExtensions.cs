0eee741e82e1ec9f95705305c3080644b516b0d7
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-07-08 23:02:54 +0500

    updated mapper contracts library

--- Api/Extensions/SlimAccommodationExtensions.cs
+++ Api/Extensions/SlimAccommodationExtensions.cs
@@ -1,5 +1,7 @@
 using HappyTravel.Edo.Api.Models.Accommodations;
+using HappyTravel.Geography;
 using HappyTravel.MapperContracts.Public.Accommodations.Internals;
+using GeoPoint = HappyTravel.Geography.GeoPoint;
 using SlimLocationInfo = HappyTravel.Edo.Api.Models.Accommodations.SlimLocationInfo;
 
 namespace HappyTravel.Edo.Api.Extensions
@@ -23,6 +25,6 @@ namespace HappyTravel.Edo.Api.Extensions
                 countryCode: locationInfo.CountryCode,
                 locality: locationInfo.Locality,
                 localityZone: locationInfo.LocalityZone,
-                coordinates: locationInfo.Coordinates);
+                coordinates: new GeoPoint(locationInfo.Coordinates.Longitude, locationInfo.Coordinates.Latitude));
     }
 }
