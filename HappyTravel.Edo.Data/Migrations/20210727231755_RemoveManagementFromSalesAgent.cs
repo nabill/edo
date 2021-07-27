a34fce0fd2c6e2dc4919fa7bd64483e15f3eb97c
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-07-28 04:20:38 +0500

    Migration to remove AgencyBookingManagement from standard agent role

---
+++ HappyTravel.Edo.Data/Migrations/20210727231755_RemoveManagementFromSalesAgent.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class RemoveManagementFromSalesAgent : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("UPDATE \"AgentRoles\" " +
+                "SET \"Permissions\" = \"Permissions\" & ~1024 " +
+                "WHERE \"Name\" ILIKE 'Sales agent'");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+            migrationBuilder.Sql("UPDATE \"AgentRoles\" " +
+                "SET \"Permissions\" = \"Permissions\" | 1024 " +
+                "WHERE \"Name\" ILIKE 'Sales agent'");
+        }
+    }
+}
