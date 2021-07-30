9ba29c1696f631763451b477189cf9707cb33edb
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-07-30 22:01:26 +0500

    Review fix

--- HappyTravel.Edo.Data/Migrations/20210727231755_RemoveManagementFromSalesAgent.cs
+++ HappyTravel.Edo.Data/Migrations/20210727231755_RemoveManagementFromSalesAgent.cs
@@ -6,17 +6,28 @@ namespace HappyTravel.Edo.Data.Migrations
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
+            //revoke from Sales agent
             migrationBuilder.Sql("UPDATE \"AgentRoles\" " +
                 "SET \"Permissions\" = \"Permissions\" & ~1024 " +
                 "WHERE \"Name\" ILIKE 'Sales agent'");
+
+            //grant to Agency administrator
+            migrationBuilder.Sql("UPDATE \"AgentRoles\" " +
+                "SET \"Permissions\" = \"Permissions\" | 1024 " +
+                "WHERE \"Name\" ILIKE 'Agency administrator'");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-
+            //grant to Sales agent
             migrationBuilder.Sql("UPDATE \"AgentRoles\" " +
                 "SET \"Permissions\" = \"Permissions\" | 1024 " +
                 "WHERE \"Name\" ILIKE 'Sales agent'");
+
+            //revoke from Agency administrator
+            migrationBuilder.Sql("UPDATE \"AgentRoles\" " +
+                "SET \"Permissions\" = \"Permissions\" & ~1024 " +
+                "WHERE \"Name\" ILIKE 'Agency administrator'");
         }
     }
 }
