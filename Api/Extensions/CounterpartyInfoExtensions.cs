0d34ce063334032d7d3190768e1394f0198847b3
Alexey Shokarev <Alexey.Shokarev@gmail.com>
2021-08-04 13:20:28 +0600

    Fixed extension formatting

--- Api/Extensions/CounterpartyInfoExtensions.cs
+++ Api/Extensions/CounterpartyInfoExtensions.cs
@@ -7,24 +7,24 @@ namespace HappyTravel.Edo.Api.Extensions
     public static class CounterpartyInfoExtensions
     {
         public static CounterpartyInfo ToCounterpartyInfo(this Counterparty counterparty, string countryNames, string languageCode, string markupFormula = null)
-            => new(counterparty.Id,
-                counterparty.Name,
-                counterparty.LegalAddress,
-                counterparty.Address,
-                counterparty.BillingEmail,
-                counterparty.City,
-                counterparty.CountryCode,
-                LocalizationHelper.GetValueFromSerializedString(countryNames, languageCode),
-                counterparty.Fax,
-                counterparty.Phone,
-                counterparty.PostalCode,
-                counterparty.Website,
-                counterparty.VatNumber,
-                counterparty.PreferredPaymentMethod,
-                counterparty.IsContractUploaded,
-                counterparty.State,
-                counterparty.Verified,
-                counterparty.IsActive,
-                markupFormula);
+            => new (id: counterparty.Id,
+                name: counterparty.Name,
+                legalAddress: counterparty.LegalAddress,
+                address: counterparty.Address,
+                billingEmail: counterparty.BillingEmail,
+                city: counterparty.City,
+                countryCode: counterparty.CountryCode,
+                countryName: LocalizationHelper.GetValueFromSerializedString(countryNames, languageCode),
+                fax: counterparty.Fax,
+                phone: counterparty.Phone,
+                postalCode: counterparty.PostalCode,
+                website: counterparty.Website,
+                vatNumber: counterparty.VatNumber,
+                preferredPaymentMethod: counterparty.PreferredPaymentMethod,
+                isContractUploaded: counterparty.IsContractUploaded,
+                verificationState: counterparty.State,
+                verificationDate: counterparty.Verified,
+                isActive: counterparty.IsActive,
+                markupFormula: markupFormula);
     }
 }
