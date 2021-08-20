6cbb1e84eb23f788e3996cbac8fda91defbe2f68
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-08-20 14:49:20 +0500

    fixed BillingAddress.cs

--- Api/Models/Payments/NGenius/BillingAddress.cs
+++ Api/Models/Payments/NGenius/BillingAddress.cs
@@ -2,9 +2,7 @@ namespace HappyTravel.Edo.Api.Models.Payments.NGenius
 {
     public struct BillingAddress
     {
-        public string Pan { get; init; }
-        public string Expiry { get; init; }
-        public string Cvv { get; init; }
-        public string CardholderName { get; init; }
+        public string FirstName { get; init; }
+        public string LastName { get; init; }
     }
 }
