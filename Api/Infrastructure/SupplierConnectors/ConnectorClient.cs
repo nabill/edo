861890fd935b938ee141753a79ec8987dc95f2ea
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-09-02 01:28:56 +0500

    Avoid trying to create Result with empty error

--- Api/Infrastructure/SupplierConnectors/ConnectorClient.cs
+++ Api/Infrastructure/SupplierConnectors/ConnectorClient.cs
@@ -97,9 +97,14 @@ namespace HappyTravel.Edo.Api.Infrastructure.SupplierConnectors
                         streamReader.BaseStream.Seek(0, SeekOrigin.Begin);
                         var responseBody = await streamReader.ReadToEndAsync();
                         _logger.LogConnectorClientUnexpectedResponse(response.StatusCode, requestFactory().RequestUri, responseBody);
-                        error = ProblemDetailsBuilder.Build(response.ReasonPhrase, response.StatusCode);
-                    }
 
+                        var reasonPhrase = string.IsNullOrWhiteSpace(response.ReasonPhrase)
+                            ? $"Error is not specified, status code: {response.StatusCode}"
+                            : response.ReasonPhrase;
+
+                        error = ProblemDetailsBuilder.Build(reasonPhrase, response.StatusCode);
+                    }
+                    
                     return Result.Failure<TResponse, ProblemDetails>(error);
                 }
 
