537b5d16dd29e99dd31698ebbf063422a49c96a3
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-09-04 13:56:17 +0500

    Fix wrong percentage in booking cancellation

--- Api/Services/SupplierOrders/SupplierOrderService.cs
+++ Api/Services/SupplierOrders/SupplierOrderService.cs
@@ -61,7 +61,7 @@ namespace HappyTravel.Edo.Api.Services.SupplierOrders
                 .FirstOrDefault();
 
             if (applyingPolicy is not null)
-                orderToCancel.RefundableAmount = (decimal) (1 - applyingPolicy.Percentage) * orderToCancel.Price;
+                orderToCancel.RefundableAmount = (decimal) (100 - applyingPolicy.Percentage) * orderToCancel.Price;
 
             orderToCancel.State = SupplierOrderState.Canceled;
             _context.SupplierOrders.Update(orderToCancel);
