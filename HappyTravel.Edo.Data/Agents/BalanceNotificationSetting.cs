19325ea1c027e2ba4aca4f4bac41e5872eed8e78
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-09-09 06:40:36 +0500

    Add table BalanceNotificationSettings migration

---
+++ HappyTravel.Edo.Data/Agents/BalanceNotificationSetting.cs
@@ -0,0 +1,9 @@
+﻿namespace HappyTravel.Edo.Data.Agents
+{
+    public class BalanceNotificationSetting
+    {
+        public int Id { get; set; }
+        public int AccountId { get; set; }
+        public int[] Thresholds { get; set; }
+    }
+}
