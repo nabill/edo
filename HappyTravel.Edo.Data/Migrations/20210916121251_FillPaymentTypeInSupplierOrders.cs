e07336ce8fb11b6518dea5977d7b3db8b3ded373
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-09-16 17:44:17 +0500

    filled payment type in supplier orders

---
+++ HappyTravel.Edo.Data/Migrations/20210916121251_FillPaymentTypeInSupplierOrders.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FillPaymentTypeInSupplierOrders : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""SupplierOrders"" SET ""PaymentType"" = CASE WHEN EXISTS (
+                    SELECT * FROM ""Bookings"" WHERE 
+                        ""Bookings"".""ReferenceCode"" = ""SupplierOrders"".""ReferenceCode"" AND
+                        ""Bookings"".""Supplier"" = 8 AND
+                        ARRAY_TO_STRING(""Tags"", ',') ILIKE '% vcc %'
+                ) THEN 2 ELSE 1 END;
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"UPDATE ""SupplierOrders"" SET ""PaymentType"" = 0");
+        }
+    }
+}
