55bc194c6bd6f276186f39e8e241c304a3c3a87a
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-09-20 17:12:04 +0500

    added handling deadline percentage greater than 100

--- Api/Services/Accommodations/Availability/Steps/WideAvailabilitySearch/RedisWideAvailabilityStorage.cs
+++ Api/Services/Accommodations/Availability/Steps/WideAvailabilitySearch/RedisWideAvailabilityStorage.cs
@@ -64,6 +64,8 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Availability.Steps.WideAva
                             : (Suppliers?)null,
                         a.HtId);
                 })
+                .Where(a => a.RoomContractSets.All(rcs => rcs.Deadline.Policies
+                    .All(p => p.Percentage <= 100)))
                 .AsQueryable();
             
             return filters.ApplyTo(queriable).ToList();
