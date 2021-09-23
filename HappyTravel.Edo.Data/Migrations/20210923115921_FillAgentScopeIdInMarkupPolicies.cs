b9914ee5568650441f5a2b1576a275d66ab68851
Konstantin Yefremov <konstyefremov@gmail.com>
2021-09-24 02:16:47 +0500

    Fix migration

--- HappyTravel.Edo.Data/Migrations/20210923115921_FillAgentScopeIdInMarkupPolicies.cs
+++ HappyTravel.Edo.Data/Migrations/20210923115921_FillAgentScopeIdInMarkupPolicies.cs
@@ -7,18 +7,17 @@ namespace HappyTravel.Edo.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"
-                UPDATE ""MarkupPolicies"" SET ""AgentScopeId"" = ""CounterPartyId""
-                    WHERE ""ScopeType"" = 2
-                UPDATE ""MarkupPolicies"" SET ""AgentScopeId"" = ""AgencyId""
-                    WHERE ""ScopeType"" = 3
-                UPDATE ""MarkupPolicies"" SET ""AgentScopeId"" = CONCAT(""AgencyId"", '-', ""AgentId"")
-                    WHERE ""ScopeType"" = 4
+                UPDATE ""MarkupPolicies"" SET ""AgentScopeType"" = ""ScopeType"";
+                UPDATE ""MarkupPolicies"" SET ""AgentScopeId"" = ""CounterpartyId"" WHERE ""ScopeType"" = 2;
+                UPDATE ""MarkupPolicies"" SET ""AgentScopeId"" = ""AgencyId"" WHERE ""ScopeType"" = 3;
+                UPDATE ""MarkupPolicies"" SET ""AgentScopeId"" = CONCAT(""AgencyId"", '-', ""AgentId"") WHERE ""ScopeType"" = 4;
             ");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
         {
             migrationBuilder.Sql(@"UPDATE ""MarkupPolicies"" SET ""AgentScopeId"" = null");
+            migrationBuilder.Sql(@"UPDATE ""MarkupPolicies"" SET ""AgentScopeType"" = 0");
         }
     }
 }
