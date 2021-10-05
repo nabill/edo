b14cf2e68d82144bb46fd1a5576e6c1d432790e8
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-10-05 11:39:30 +0500

    added in-memory markup policy storage

---
+++ Api/Services/Markups/IMarkupPolicyStorage.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using HappyTravel.Edo.Data.Markup;
+
+namespace HappyTravel.Edo.Api.Services.Markups
+{
+    public interface IMarkupPolicyStorage
+    {
+        Task<List<MarkupPolicy>> Get(Func<MarkupPolicy, bool> predicate);
+    }
+}
