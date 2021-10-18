9656febf961cb62f7ea9a0444a06a428fbda226d
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-10-18 13:25:33 +0500

    replace agency id in markup bonus display service

--- Api/Services/Markups/MarkupBonusDisplayService.cs
+++ Api/Services/Markups/MarkupBonusDisplayService.cs
@@ -1,9 +1,10 @@
-using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using HappyTravel.Edo.Api.Models.Agents;
+using HappyTravel.Edo.Common.Enums.Markup;
 using HappyTravel.Edo.Data;
 using HappyTravel.Money.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace HappyTravel.Edo.Api.Services.Markups
 {
@@ -17,47 +18,40 @@ namespace HappyTravel.Edo.Api.Services.Markups
         
         public IQueryable<Bonus> GetBonuses(AgentContext agentContext)
         {
-            // TODO: https://github.com/happy-travel/agent-app-project/issues/731
-            return new List<Bonus>().AsQueryable();
-            // return from appliedMarkup in _context.AppliedBookingMarkups
-            //    join markupPolicy in _context.MarkupPolicies on appliedMarkup.PolicyId equals markupPolicy.Id
-            //     join agentAgency in _context.AgentAgencyRelations on markupPolicy.AgentId equals agentAgency.AgentId
-            //     join booking in _context.Bookings on appliedMarkup.ReferenceCode equals booking.ReferenceCode
-            //     where agentAgency.AgencyId == agentContext.AgencyId
-            //     select new Bonus
-            //     {
-            //         ReferenceCode = appliedMarkup.ReferenceCode,
-            //         Created = booking.Created,
-            //         Paid = appliedMarkup.Paid,
-            //         Amount = new MoneyAmount(appliedMarkup.Amount, markupPolicy.Currency)
-            //     };
+            return from appliedMarkup in _context.AppliedBookingMarkups
+                join markupPolicy in _context.MarkupPolicies on appliedMarkup.PolicyId equals markupPolicy.Id
+                join booking in _context.Bookings on appliedMarkup.ReferenceCode equals booking.ReferenceCode
+                where markupPolicy.AgentScopeId == agentContext.AgencyId.ToString() && markupPolicy.AgentScopeType == AgentMarkupScopeTypes.Agency
+                select new Bonus
+                {
+                    ReferenceCode = appliedMarkup.ReferenceCode,
+                    Created = booking.Created,
+                    Paid = appliedMarkup.Paid,
+                    Amount = new MoneyAmount(appliedMarkup.Amount, markupPolicy.Currency)
+                };
         }
 
 
         public async Task<BonusSummary> GetBonusesSummary(BonusSummaryFilter filter, AgentContext agentContext)
         {
-            // TODO: https://github.com/happy-travel/agent-app-project/issues/731
-            return new BonusSummary();
+            var query = from appliedMarkup in _context.AppliedBookingMarkups
+                join markupPolicy in _context.MarkupPolicies on appliedMarkup.PolicyId equals markupPolicy.Id
+                where markupPolicy.AgentScopeId == agentContext.AgencyId.ToString() && markupPolicy.AgentScopeType == AgentMarkupScopeTypes.Agency 
+                select new {appliedMarkup.Paid, appliedMarkup.Amount, markupPolicy.Currency};
+            
 
-            // var query = from appliedMarkup in _context.AppliedBookingMarkups
-            //     join markupPolicy in _context.MarkupPolicies on appliedMarkup.PolicyId equals markupPolicy.Id
-            //     join agentAgency in _context.AgentAgencyRelations on markupPolicy.AgentId equals agentAgency.AgentId
-            //     where agentAgency.AgencyId == agentContext.AgencyId
-            //     select new {appliedMarkup.Paid, appliedMarkup.Amount, markupPolicy.Currency};
-            // 
+            if (filter.From is not null)
+                query = query.Where(x => x.Paid >= filter.From.Value);
 
-            // if (filter.From is not null)
-            //     query = query.Where(x => x.Paid >= filter.From.Value);
+            if (filter.To is not null)
+                query = query.Where(x => x.Paid <= filter.To.Value);
 
-            // if (filter.To is not null)
-            //     query = query.Where(x => x.Paid <= filter.To.Value);
+            var summary = await query
+                .GroupBy(x => x.Currency)
+                .Select(x => new MoneyAmount (x.Sum(g => g.Amount), x.Key))
+                .ToListAsync();
 
-            // var summary = await query
-            //     .GroupBy(x => x.Currency)
-            //     .Select(x => new MoneyAmount (x.Sum(g => g.Amount), x.Key))
-            //     .ToListAsync();
-
-            // return new BonusSummary { Summary = summary };
+            return new BonusSummary { Summary = summary };
         }
 
 
