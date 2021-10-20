217cf7287a48385da251663e68b82ef8c9eedd97
Konstantin Yefremov <konstyefremov@gmail.com>
2021-10-20 16:29:31 +0500

    Cleanup code

--- HappyTravel.Edo.Common/Enums/Markup/AgentMarkupScopeTypes.cs
+++ HappyTravel.Edo.Common/Enums/Markup/AgentMarkupScopeTypes.cs
@@ -3,15 +3,15 @@ using Newtonsoft.Json.Converters;
 
 namespace HappyTravel.Edo.Common.Enums.Markup
 {
-    [JsonConverter((typeof(StringEnumConverter)))]
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum AgentMarkupScopeTypes
     {
         NotSpecified = 0,
         Global = 1,
         Country = 2,
         City = 3,
-        Counterparty = 4, // 2
-        Agency = 5, // 3
-        Agent = 6 // 4
+        Counterparty = 4,
+        Agency = 5,
+        Agent = 6
     }
 }
