a59b10ece0ea56f4b9eb827ad0d088e8c52522db
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-11-02 17:08:32 +0500

    2

--- HappyTravel.Edo.DirectApi/Services/AccommodationBookingInfoExtensions.cs
+++ HappyTravel.Edo.DirectApi/Services/AccommodationBookingInfoExtensions.cs
@@ -1,4 +1,5 @@
-﻿using HappyTravel.Edo.DirectApi.Models;
+﻿using HappyTravel.Edo.Data.Bookings;
+using HappyTravel.Edo.DirectApi.Models;
 
 namespace HappyTravel.Edo.DirectApi.Services
 {
@@ -21,7 +22,24 @@ namespace HappyTravel.Edo.DirectApi.Services
 
         private static AccommodationBookingDetails FromEdoModel(this Edo.Api.Models.Bookings.AccommodationBookingDetails bookingDetails)
         {
-            return new AccommodationBookingDetails();
+            return new AccommodationBookingDetails(referenceCode: bookingDetails.ReferenceCode,
+                agentReference: bookingDetails.AgentReference,
+                status: bookingDetails.Status,
+                numberOfNights: bookingDetails.NumberOfNights,
+                checkInDate: bookingDetails.CheckInDate,
+                checkOutDate: bookingDetails.CheckOutDate,
+                location: bookingDetails.Location,
+                contactInfo: bookingDetails.ContactInfo,
+                accommodationId: bookingDetails.AccommodationId,
+                accommodationName: bookingDetails.AccommodationName,
+                accommodationInfo: bookingDetails.AccommodationInfo,
+                deadlineDate: bookingDetails.DeadlineDate,
+                roomDetails: bookingDetails.RoomDetails,
+                numberOfPassengers: bookingDetails.NumberOfPassengers,
+                cancellationPolicies: bookingDetails.CancellationPolicies,
+                created: bookingDetails.Created,
+                propertyOwnerConfirmationCode: bookingDetails.PropertyOwnerConfirmationCode,
+                isAdvancePurchaseRate: );
         }
     }
 }
