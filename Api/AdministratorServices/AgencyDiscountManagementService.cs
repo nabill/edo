58f2007c2a3232a4896df0dbc6bf789b5e363716
Konstantin Yefremov <konstyefremov@gmail.com>
2021-11-02 14:58:34 +0500

    Change condition to improve readability

--- Api/AdministratorServices/AgencyDiscountManagementService.cs
+++ Api/AdministratorServices/AgencyDiscountManagementService.cs
@@ -76,13 +76,13 @@ namespace HappyTravel.Edo.Api.AdministratorServices
                 if (targetMarkup is null)
                     return Result.Failure($"Could not find markup policy with id {createDiscountRequest.TargetMarkupId}");
 
-                if (targetMarkup.SubjectScopeType != SubjectMarkupScopeTypes.Global
-                    && targetMarkup.DestinationScopeType != DestinationMarkupScopeTypes.Global)
+                if (targetMarkup.SubjectScopeType == SubjectMarkupScopeTypes.Global
+                    && targetMarkup.DestinationScopeType == DestinationMarkupScopeTypes.Global)
                 {
-                    return Result.Failure("Cannot apply discount to non-global markup policy");
+                    return Result.Success();
                 }
 
-                return Result.Success();
+                return Result.Failure("Cannot apply discount to non-global markup policy");
             }
 
 
