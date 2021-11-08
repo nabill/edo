ba0e9bb8e98b95b2903b409e5ffcbd22e2864f44
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-11-08 20:39:49 +0500

    moved to extension method

--- Api/Services/Accommodations/Bookings/BookingExecution/Flows/BankCreditCardBookingFlow.cs
+++ Api/Services/Accommodations/Bookings/BookingExecution/Flows/BankCreditCardBookingFlow.cs
@@ -43,7 +43,7 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Bookings.BookingExecution.
         
         public async Task<Result<string>> Register(AccommodationBookingRequest bookingRequest, AgentContext agentContext, string languageCode)
         {
-            using var _ = _logger.BeginScope(new Dictionary<string, object> {{"SearchId", bookingRequest.SearchId}});
+            using var _ = _logger.AddSearchIdScope(bookingRequest.SearchId);
             
             var (_, isFailure, booking, error) = await GetCachedAvailability(bookingRequest)
                 .Ensure(IsPaymentTypeAllowed, "Payment type is not allowed")
