9bf080878768f624546a8b29119750338aa3055d
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-11-09 17:39:04 +0500

    added automated adding supplier connector clients

--- Api/Infrastructure/Options/SupplierOptions.cs
+++ Api/Infrastructure/Options/SupplierOptions.cs
@@ -5,22 +5,7 @@ namespace HappyTravel.Edo.Api.Infrastructure.Options
 {
     public class SupplierOptions
     {
-        public string Netstorming { get; set; }
-        public string Illusions { get; set; }
-        public string Etg { get; set; }
-        public string DirectContracts { get; set; }
-        public string Rakuten { get; set; }
-        public string Columbus { get; set; }
-        public string TravelgateXTest { get; set; }
-        public string Darina { get; set; }
-        public string Jumeirah { get; set; }
-        public string Paximum { get; set; }
-        public string Yalago { get; set; }
-        public string HotelBeds { get; set; }
-        public string Roibos { get; set; }
-        public string Bronevik { get; set; }
-        public string HotelbookPro { get; set; }
-        public string BookMe { get; set; }
         public List<Suppliers> EnabledSuppliers { get; set; }
+        public Dictionary<Suppliers, string> Endpoints { get; set; }
     }
 }
--- Api/Services/Connectors/SupplierConnectorManager.cs
+++ Api/Services/Connectors/SupplierConnectorManager.cs
@@ -1,13 +1,7 @@
 using System;
 using System.Collections.Generic;
-using System.Threading.Tasks;
-using FloxDc.CacheFlow;
-using FloxDc.CacheFlow.Extensions;
 using HappyTravel.Edo.Api.Infrastructure.Options;
 using HappyTravel.Edo.Api.Infrastructure.SupplierConnectors;
-using HappyTravel.Edo.Api.Models.Agents;
-using HappyTravel.Edo.Api.Services.Agents;
-using HappyTravel.Edo.Common.Enums;
 using HappyTravel.SuppliersCatalog;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
@@ -17,84 +11,42 @@ namespace HappyTravel.Edo.Api.Services.Connectors
 {
     public class SupplierConnectorManager : ISupplierConnectorManager
     {
-        public SupplierConnectorManager(IOptions<SupplierOptions> options,
-            IConnectorClient connectorClient,
-            IServiceProvider serviceProvider)
+        public SupplierConnectorManager(IOptionsMonitor<SupplierOptions> options, IServiceProvider serviceProvider)
         {
-            _options = options.Value;
-            _suppliers = new Dictionary<Suppliers, ISupplierConnector>
-            {
-                // TODO: Add other suppliers.
-                {
-                    Suppliers.Netstorming,
-                    new SupplierConnector(Suppliers.Netstorming, connectorClient, _options.Netstorming, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Illusions,
-                    new SupplierConnector(Suppliers.Illusions, connectorClient, _options.Illusions, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Etg,
-                    new SupplierConnector(Suppliers.Etg, connectorClient, _options.Etg, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.DirectContracts,
-                    new SupplierConnector(Suppliers.DirectContracts, connectorClient, _options.DirectContracts, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Rakuten,
-                    new SupplierConnector(Suppliers.Rakuten, connectorClient, _options.Rakuten, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Columbus,
-                    new SupplierConnector(Suppliers.Columbus, connectorClient, _options.Columbus, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.TravelgateXTest,
-                    new SupplierConnector(Suppliers.TravelgateXTest, connectorClient, _options.TravelgateXTest, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Darina,
-                    new SupplierConnector(Suppliers.Darina, connectorClient, _options.Darina, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Jumeirah,
-                    new SupplierConnector(Suppliers.Jumeirah, connectorClient, _options.Jumeirah, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Paximum,
-                    new SupplierConnector(Suppliers.Paximum, connectorClient, _options.Paximum, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Yalago,
-                    new SupplierConnector(Suppliers.Yalago, connectorClient, _options.Yalago, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.HotelBeds,
-                    new SupplierConnector(Suppliers.HotelBeds, connectorClient, _options.HotelBeds, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Roibos,
-                    new SupplierConnector(Suppliers.Roibos, connectorClient, _options.Roibos, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.Bronevik,
-                    new SupplierConnector(Suppliers.Bronevik, connectorClient, _options.Bronevik, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.HotelbookPro,
-                    new SupplierConnector(Suppliers.HotelbookPro, connectorClient, _options.HotelbookPro, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                },
-                {
-                    Suppliers.BookMe,
-                    new SupplierConnector(Suppliers.BookMe, connectorClient, _options.BookMe, serviceProvider.GetRequiredService<ILogger<SupplierConnector>>())
-                }
-            };
+            _options = options;
+            _serviceProvider = serviceProvider;
+            _options.OnChange(_ => UpdateConnectorClients());
+            
+            UpdateConnectorClients();
         }
 
-        public ISupplierConnector Get(Suppliers key) => _suppliers[key];
+
+        private void UpdateConnectorClients()
+        {
+            var connectorClient = _serviceProvider.GetRequiredService<IConnectorClient>();
+            var logger = _serviceProvider.GetRequiredService<ILogger<SupplierConnector>>();
+            var suppliers = new Dictionary<Suppliers, ISupplierConnector>();
+            
+            foreach (var (supplier, endpoint) in _options.CurrentValue.Endpoints)
+            {
+                suppliers.Add(supplier, new SupplierConnector(
+                    supplier: supplier, 
+                    connectorClient: connectorClient, 
+                    baseUrl: endpoint, 
+                    logger: logger));
+            }
+
+            _suppliers = suppliers;
+        }
         
-        private readonly Dictionary<Suppliers, ISupplierConnector> _suppliers;
-        private readonly SupplierOptions _options;
+
+        public ISupplierConnector Get(Suppliers key) => _suppliers[key];
+
+        
+        private Dictionary<Suppliers, ISupplierConnector> _suppliers = new();
+        
+        
+        private readonly IOptionsMonitor<SupplierOptions> _options;
+        private readonly IServiceProvider _serviceProvider;
     }
 }
