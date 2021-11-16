1c2ecb8928a1381fb98f22f610e31b7341814ffa
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-11-16 09:28:26 +0500

    fix saving nationality and residency in wrong case

---
+++ HappyTravel.Edo.Data/Migrations/20211116040927_FixNationalityCase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FixNationalityCase : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""Bookings"" 
+                    SET ""Nationality"" = upper(""Nationality"")
+                WHERE char_length(""Nationality"") = 2
+            ");
+            
+            migrationBuilder.Sql(@"
+                UPDATE ""Bookings"" 
+                    SET ""Residency"" = upper(""Residency"")
+                WHERE char_length(""Residency"") = 2
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
