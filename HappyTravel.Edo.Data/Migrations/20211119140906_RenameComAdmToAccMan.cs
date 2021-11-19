50efc71800a5124f55b22f1cf2ab838842e1d776
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-11-19 19:11:13 +0500

    Rename 'Company administrator' to 'Account manager'

---
+++ HappyTravel.Edo.Data/Migrations/20211119140906_RenameComAdmToAccMan.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class RenameComAdmToAccMan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update \"AdministratorRoles\" " +
+                "set \"Name\" = 'Account manager' " +
+                "where \"Id\" = 1");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
