8ce848c1d5044fd576d6db5dc214fd272e53dfd3
oleg1std <konovalov.onk@gmail.com>
2021-11-19 14:15:48 +0300

    Modify the permissions of the Accommodation Mapping Manager role

---
+++ HappyTravel.Edo.Data/Migrations/20211119111110_ModifyAccommodationMappingManagerRole.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class ModifyAccommodationMappingManagerRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                UPDATE ""AdministratorRoles"" 
+                SET ""Permissions"" = 132096
+                WHERE ""Id"" = 6
+            ");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
