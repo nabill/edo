f3e8dfc0e808bdcaeef27c92ce3781d2acaa0814
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-11-22 17:15:21 +0500

    Added notifications to administrator role

---
+++ HappyTravel.Edo.Data/Migrations/20211122121409_AddAdmRoleNotifications.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class AddAdmRoleNotifications : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int[]>(
+                name: "NotificationTypes",
+                table: "AdministratorRoles",
+                type: "integer[]",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NotificationTypes",
+                table: "AdministratorRoles");
+        }
+    }
+}
