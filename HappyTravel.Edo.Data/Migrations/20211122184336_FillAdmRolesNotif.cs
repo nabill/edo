73b00de96298ede5ff8a1b911a417a7a6cd025e5
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-11-22 23:44:39 +0500

    Fill administrator roles notifications

---
+++ HappyTravel.Edo.Data/Migrations/20211122184336_FillAdmRolesNotif.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FillAdmRolesNotif : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("update \"AdministratorRoles\" set \"NotificationTypes\" = '{16, 17}' where \"Id\" = 1");
+            migrationBuilder.Sql("update \"AdministratorRoles\" set \"NotificationTypes\" = '{4, 19, 22}' where \"Id\" = 2");
+            migrationBuilder.Sql("update \"AdministratorRoles\" set \"NotificationTypes\" = '{2, 6, 7, 23, 18}' where \"Id\" = 3");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
