292002508f6a90451b62d897ffb09c1e57851cda
AlexeyShok <Alexey.Shokarev@gmail.com>
2021-11-23 22:15:36 +0600

    Added migration

---
+++ HappyTravel.Edo.Data/Migrations/20211123155810_FixPreviousMigration.cs
@@ -0,0 +1,28 @@
+﻿using HappyTravel.Edo.Notifications.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FixPreviousMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var table = "DefaultNotificationOptions";
+            var keyColumn = "Type";
+            migrationBuilder.UpdateData(table, keyColumn, 8, "EnabledProtocols", (int)ProtocolTypes.WebSocket);
+            migrationBuilder.UpdateData(table, keyColumn, 8, "AdminEmailTemplateId", null);
+
+
+            migrationBuilder.InsertData("DefaultNotificationOptions", new string[] { "Type", "EnabledProtocols", "IsMandatory", "EnabledReceivers", "AgentEmailTemplateId", "AdminEmailTemplateId", "PropertyOwnerEmailTemplateId" },
+                new object[,]
+                {
+                    { (int)NotificationTypes.BookingStatusChangedToPendingOrWaitingForResponse, (int)(ProtocolTypes.Email), false, (int)ReceiverTypes.AdminPanel }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
