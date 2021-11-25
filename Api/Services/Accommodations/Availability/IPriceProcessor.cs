bcfdb74628aaafff3ca8f74a147b96136ce8e78f
mingazhev <43397444+mingazhev@users.noreply.github.com>
2021-11-25 15:46:37 +0500

    Improved markup applying performance with not getting agency on each markup calculation

--- Api/Services/Accommodations/Availability/IPriceProcessor.cs
+++ Api/Services/Accommodations/Availability/IPriceProcessor.cs
@@ -1,7 +1,6 @@
 using System;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
-using HappyTravel.Edo.Api.Models.Agents;
 using HappyTravel.Edo.Api.Services.Markups.Abstractions;
 using HappyTravel.Edo.Api.Services.PriceProcessing;
 using HappyTravel.Money.Enums;
@@ -14,7 +13,7 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Availability
         Task<Result<TDetails, ProblemDetails>> ConvertCurrencies<TDetails>(TDetails details, Func<TDetails, PriceProcessFunction, ValueTask<TDetails>> changePricesFunc, Func<TDetails, Currencies?> getCurrencyFunc);
 
 
-        Task<TDetails> ApplyMarkups<TDetails>(AgentContext agent, TDetails details,
+        Task<TDetails> ApplyMarkups<TDetails>(MarkupSubjectInfo subjectInfo, TDetails details,
             Func<TDetails, PriceProcessFunction, ValueTask<TDetails>> priceProcessFunc,
             Func<TDetails, MarkupDestinationInfo> getMarkupDestinationFunc = null,
             Action<MarkupApplicationResult<TDetails>> logAction = null);
