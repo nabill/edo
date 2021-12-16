ef4d65396f2d74e7378532015e8dbb9d634dcee2
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-12-16 16:45:41 +0500

    renamed method

--- Api/Services/Accommodations/Availability/Steps/WideAvailabilitySearch/WideAvailabilitySearchService.cs
+++ Api/Services/Accommodations/Availability/Steps/WideAvailabilitySearch/WideAvailabilitySearchService.cs
@@ -89,7 +89,7 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Availability.Steps.WideAva
         {
             foreach (var supplier in searchSettings.EnabledConnectors)
             {
-                if (!IsMultiRoomBookingAllowed(supplier) || !accommodationCodes.TryGetValue(supplier, out var supplierCodeMappings))
+                if (!IsBookingAllowed(supplier) || !accommodationCodes.TryGetValue(supplier, out var supplierCodeMappings))
                 {
                     await _stateStorage.SaveState(searchId, SupplierAvailabilitySearchState.Completed(searchId, new List<string>(0), 0), supplier);
                     continue;
@@ -113,7 +113,7 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Availability.Steps.WideAva
             }
 
 
-            bool IsMultiRoomBookingAllowed(Suppliers supplier)
+            bool IsBookingAllowed(Suppliers supplier)
                 => request.RoomDetails.Count == 1 || searchSettings.AllowedMultiRoomBookingSuppliers.Contains(supplier);
         }
         
