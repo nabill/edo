d6e5d1f720279c888dfaf15981a286a0dd47f986
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-12-16 09:41:18 +0500

    added constrains to geo point

--- HappyTravel.Edo.DirectApi/Models/Static/GeoPoint.cs
+++ HappyTravel.Edo.DirectApi/Models/Static/GeoPoint.cs
@@ -1,11 +1,12 @@
-﻿using System.Text.Json.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
+using System.Text.Json.Serialization;
 
 namespace HappyTravel.Edo.DirectApi.Models.Static
 {
     public readonly struct GeoPoint
     {
         [JsonConstructor]
-        public GeoPoint(double longitude, double latitude)
+        public GeoPoint([Range(-180, 180)]double longitude, [Range(-90, 90)]double latitude)
         {
             Latitude = latitude;
             Longitude = longitude;
