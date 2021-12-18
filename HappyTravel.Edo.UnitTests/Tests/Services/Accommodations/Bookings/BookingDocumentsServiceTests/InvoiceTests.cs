adcc4c41b14a225782b4a6d456a1fd475bf41dd0
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-12-18 13:29:44 +0500

    fixed tests

--- HappyTravel.Edo.UnitTests/Tests/Services/Accommodations/Bookings/BookingDocumentsServiceTests/InvoiceTests.cs
+++ HappyTravel.Edo.UnitTests/Tests/Services/Accommodations/Bookings/BookingDocumentsServiceTests/InvoiceTests.cs
@@ -1,30 +1,22 @@
 using System;
 using System.Collections.Generic;
-using System.Threading;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
 using HappyTravel.Edo.Api.AdministratorServices;
 using HappyTravel.Edo.Api.Infrastructure.Options;
 using HappyTravel.Edo.Api.Models.Bookings;
-using HappyTravel.Edo.Api.Services.Accommodations;
 using HappyTravel.Edo.Api.Services.Accommodations.Availability.Mapping;
 using HappyTravel.Edo.Api.Services.Accommodations.Bookings.Documents;
-using HappyTravel.Edo.Api.Services.Agents;
 using HappyTravel.Edo.Api.Services.Documents;
 using HappyTravel.Edo.Api.Services.Files;
 using HappyTravel.Edo.Common.Enums;
 using HappyTravel.Edo.Data.Bookings;
 using HappyTravel.Edo.Data.Documents;
 using HappyTravel.Edo.UnitTests.Utility;
-using HappyTravel.MapperContracts.Public.Accommodations;
-using HappyTravel.MapperContracts.Public.Accommodations.Enums;
-using HappyTravel.MapperContracts.Public.Accommodations.Internals;
 using HappyTravel.Money.Models;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using Moq;
 using Xunit;
-using ImageInfo = HappyTravel.MapperContracts.Public.Accommodations.Internals.ImageInfo;
 
 namespace HappyTravel.Edo.UnitTests.Tests.Services.Accommodations.Bookings.BookingDocumentsServiceTests
 {
@@ -72,44 +64,14 @@ namespace HappyTravel.Edo.UnitTests.Tests.Services.Accommodations.Bookings.Booki
                 }
                 : new List<(DocumentRegistrationInfo Metadata, BookingInvoiceData Data)>();
 
-            var accommodation = new Accommodation(htId: string.Empty,
-                name: string.Empty,
-                accommodationAmenities: new List<string>(0),
-                additionalInfo: new Dictionary<string, string>(),
-                category: string.Empty,
-                contacts: new ContactInfo(),
-                location: new LocationInfo(countryCode: string.Empty,
-                    countryHtId: string.Empty,
-                    country: string.Empty,
-                    localityHtId: string.Empty,
-                    locality: string.Empty,
-                    localityZoneHtId: string.Empty,
-                    localityZone: string.Empty,
-                    coordinates: new GeoPoint(0d, 0d),
-                    address: string.Empty,
-                    locationDescriptionCode: new LocationDescriptionCodes(),
-                    new List<PoiInfo>(0)),
-                photos: new List<ImageInfo>(),
-                AccommodationRatings.NotRated,
-                schedule: new ScheduleInfo(checkInTime: string.Empty,
-                    checkOutTime: string.Empty),
-                textualDescriptions: new List<TextualDescription>(0),
-                type: PropertyTypes.Hotels,
-                suppliers: new List<SupplierInfo>(0),
-                modified: DateTime.UtcNow);
-
             var invoiceServiceMock = new Mock<IInvoiceService>();
             invoiceServiceMock.Setup(i => i.Get<BookingInvoiceData>(It.IsAny<ServiceTypes>(), It.IsAny<ServiceSource>(), It.IsAny<string>()))
                 .ReturnsAsync(invoices);
 
-            var accommodationMapperClient = new Mock<IAccommodationMapperClient>();
-            accommodationMapperClient.Setup(c => c.GetAccommodation(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<CancellationToken>()))
-                .ReturnsAsync(accommodation);
-
             return new BookingDocumentsService(
                 edoContext.Object,
                 Mock.Of<IOptions<BankDetails>>(),
-                accommodationMapperClient.Object,
+                Mock.Of<IAccommodationMapperClient>(),
                 invoiceServiceMock.Object,
                 Mock.Of<IReceiptService>(),
                 Mock.Of<IImageFileService>(),
