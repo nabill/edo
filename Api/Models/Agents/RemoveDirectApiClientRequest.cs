bb52425606be523f28fd87cd4ab1383f699abb9f
bogdan.kushnir <bogdan.kushnir@happytravel.com>
2021-12-20 16:01:24 +0500

    Add AgencyId into the AgentDirectApiClientRelations table

--- Api/Models/Agents/RemoveDirectApiClientRequest.cs
+++ Api/Models/Agents/RemoveDirectApiClientRequest.cs
@@ -5,13 +5,15 @@ namespace HappyTravel.Edo.Api.Models.Agents
     public readonly struct RemoveDirectApiClientRequest
     {
         [JsonConstructor]
-        public RemoveDirectApiClientRequest(int agentId, string clientId)
+        public RemoveDirectApiClientRequest(int agentId, int agencyId, string clientId)
         {
             AgentId = agentId;
+            AgencyId = agencyId;
             ClientId = clientId;
         }
 
         public int AgentId { get; }
+        public int AgencyId { get; }
         public string ClientId { get; }
     }
 }
