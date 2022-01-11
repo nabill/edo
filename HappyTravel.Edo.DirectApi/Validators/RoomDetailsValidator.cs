8c031e56085b8b19aea4854b40d4f0d97b4b9e54
Denis Nikolaev <denis.nikolaev@happytravel.com>
2022-01-11 10:38:18 +0500

    added leader checking in booking request

--- HappyTravel.Edo.DirectApi/Validators/RoomDetailsValidator.cs
+++ HappyTravel.Edo.DirectApi/Validators/RoomDetailsValidator.cs
@@ -1,5 +1,6 @@
-﻿using FluentValidation;
-using HappyTravel.Edo.DirectApi.Models;
+﻿using System.Collections.Generic;
+using System.Linq;
+using FluentValidation;
 using HappyTravel.Edo.DirectApi.Models.Booking;
 
 namespace HappyTravel.Edo.DirectApi.Validators
@@ -8,8 +9,12 @@ namespace HappyTravel.Edo.DirectApi.Validators
     {
         public RoomDetailsValidator()
         {
-            RuleFor(d => d.Passengers).NotEmpty();
+            RuleFor(d => d.Passengers).NotEmpty().Must(HasLeader).WithMessage("Passengers doesn't have a leader");
             RuleForEach(d => d.Passengers).SetValidator(new PaxValidator());
         }
+
+
+        private bool HasLeader(List<Pax> passengers) 
+            => passengers.Any(p => p.IsLeader);
     }
 }
