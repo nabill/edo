a81907f9a79c71a6ed5200d97e5e499f59158c9f
mingazhev <43397444+mingazhev@users.noreply.github.com>
2022-01-12 13:40:39 +0500

    Added payment processor to payment link

---
+++ HappyTravel.Edo.Data/Migrations/20220112083437_AddPaymentProcessorForPaymentLink.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class AddPaymentProcessorForPaymentLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PaymentProcessor",
+                table: "PaymentLinks",
+                type: "integer",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PaymentProcessor",
+                table: "PaymentLinks");
+        }
+    }
+}
