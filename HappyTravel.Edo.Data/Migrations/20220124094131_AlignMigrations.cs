0849231b53c0acede92a21f3124dc7358f799f30
mingazhev <43397444+mingazhev@users.noreply.github.com>
2022-01-24 14:43:00 +0500

    Aligned migrations

---
+++ HappyTravel.Edo.Data/Migrations/20220124094131_AlignMigrations.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class AlignMigrations : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "InvoiceNumber",
+                table: "PaymentLinks",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "InvoiceNumber",
+                table: "PaymentLinks");
+        }
+    }
+}
