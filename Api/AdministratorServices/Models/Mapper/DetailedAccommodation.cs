98aa46fce4acf2710c99f5a28e28ffe6f5f56ea5
mingazhev <43397444+mingazhev@users.noreply.github.com>
2022-02-09 13:31:59 +0500

    Changed model to use string instead of supplier enum

--- Api/AdministratorServices/Models/Mapper/DetailedAccommodation.cs
+++ Api/AdministratorServices/Models/Mapper/DetailedAccommodation.cs
@@ -6,7 +6,7 @@ namespace HappyTravel.Edo.Api.AdministratorServices.Models.Mapper
 {
     public readonly struct DetailedAccommodation
     {
-        public DetailedAccommodation(AccommodationData data, Dictionary<Suppliers, AccommodationData> suppliersRawAccommodationData, AccommodationData manualCorrectedData, Dictionary<AccommodationDataTypes, List<int>> suppliersPriorities)
+        public DetailedAccommodation(AccommodationData data, Dictionary<string, AccommodationData> suppliersRawAccommodationData, AccommodationData manualCorrectedData, Dictionary<AccommodationDataTypes, List<int>> suppliersPriorities)
         {
             Data = data;
             SuppliersRawAccommodationData = suppliersRawAccommodationData ?? new ();
@@ -16,7 +16,7 @@ namespace HappyTravel.Edo.Api.AdministratorServices.Models.Mapper
 
 
         public AccommodationData Data { get; init; }
-        public Dictionary<Suppliers, AccommodationData> SuppliersRawAccommodationData { get; init; }
+        public Dictionary<string, AccommodationData> SuppliersRawAccommodationData { get; init; }
         public AccommodationData ManualCorrectedData { get; init; }
         public Dictionary<AccommodationDataTypes, List<int>> SuppliersPriorities { get; init; }
     }
