b7a8de33f841c40ee94da1fb52e574ba018ab097
Hasmik <hasmik_harutyunyan@outlook.com>
2022-02-15 17:05:35 +0400

    added needed fields to mapper api

--- Api/AdministratorServices/Models/Mapper/DetailedAccommodation.cs
+++ Api/AdministratorServices/Models/Mapper/DetailedAccommodation.cs
@@ -5,18 +5,23 @@ namespace HappyTravel.Edo.Api.AdministratorServices.Models.Mapper
 {
     public readonly struct DetailedAccommodation
     {
-        public DetailedAccommodation(AccommodationData data, Dictionary<string, AccommodationData> suppliersRawAccommodationData, AccommodationData manualCorrectedData, Dictionary<AccommodationDataTypes, List<int>> suppliersPriorities)
+        public DetailedAccommodation(string htId, AccommodationData data, Dictionary<string, AccommodationData> suppliersRawAccommodationData,
+            AccommodationData manualCorrectedData, Dictionary<AccommodationDataTypes, List<int>> suppliersPriorities, bool isActive)
         {
             Data = data;
-            SuppliersRawAccommodationData = suppliersRawAccommodationData ?? new ();
+            SuppliersRawAccommodationData = suppliersRawAccommodationData ?? new();
             ManualCorrectedData = manualCorrectedData;
-            SuppliersPriorities = suppliersPriorities ?? new ();
+            SuppliersPriorities = suppliersPriorities ?? new();
+            HtId = htId;
+            IsActive = isActive;
         }
 
 
+        public string HtId { get; init; }
         public AccommodationData Data { get; init; }
         public Dictionary<string, AccommodationData> SuppliersRawAccommodationData { get; init; }
         public AccommodationData ManualCorrectedData { get; init; }
         public Dictionary<AccommodationDataTypes, List<int>> SuppliersPriorities { get; init; }
+        public bool IsActive { get; init; }
     }
 }
