ba49128ea657cbac014e5508205ae588c07b1bc7
Konstantin Yefremov <konstyefremov@gmail.com>
2022-02-17 18:19:43 +0500

    Fill SupplierCode in Bookings and SupplierCodes

---
+++ HappyTravel.Edo.Data/Migrations/20220217123751_FillSupplierCodesToBookingsAndSupplierCodes.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FillSupplierCodesToBookingsAndSupplierCodes : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var fromTo = new[]
+            {
+                (1, "netstorming"),
+                (2, "illusions"),
+                (3, "directContracts"),
+                (4, "etg"),
+                (5, "rakuten"),
+                (6, "columbus"),
+                (7, "travelgateXTest"),
+                (8, "jumeirah"),
+                (9, "darina"),
+                (10, "yalago"),
+                (11, "paximum"),
+                (12, "travelLine"),
+                (13, "bronevik"),
+                (14, "travelClick"),
+                (15, "sodis"),
+                (16, "hotelBeds"),
+                (17, "hotelbookPro"),
+                (18, "bookMe"),
+                (19, "roibos"),
+                (20, "sabre"),
+                (21, "bakuun"),
+                (22, "accor"),
+                (23, "nirvana"),
+                (24, "nuitee"),
+                (25, "hyperGuest"),
+                (26, "solole"),
+                (27, "grnConnect"),
+                (28, "methabook"),
+                (29, "withinearth"),
+                (30, "mtsCityBreaks"),
+                (31, "avraTours"),
+                (32, "ddHolidays")
+            };
+
+            foreach (var (from, to) in fromTo)
+            {
+                migrationBuilder.Sql($@"
+                UPDATE ""Bookings"" 
+                SET ""SupplierCode"" = '{to}'
+                WHERE ""Supplier"" = {from}");
+                
+                migrationBuilder.Sql($@"
+                UPDATE ""SupplierOrders"" 
+                SET ""SupplierCode"" = '{to}'
+                WHERE ""Supplier"" = {from}");
+            }
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        { }
+    }
+}
