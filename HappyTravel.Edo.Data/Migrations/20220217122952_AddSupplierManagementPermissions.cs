0f52b5bb1300147b9ae06c9ec6a4feb98f4a0a08
mingazhev <43397444+mingazhev@users.noreply.github.com>
2022-02-17 17:35:31 +0500

    Added supplier management controller

---
+++ HappyTravel.Edo.Data/Migrations/20220217122952_AddSupplierManagementPermissions.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class AddSupplierManagementPermissions : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var sql = "update \"AdministratorRoles\" set \"Permissions\" = \"Permissions\" | 16777216 where \"Name\" = 'System Administrator';";
+            migrationBuilder.Sql(sql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
