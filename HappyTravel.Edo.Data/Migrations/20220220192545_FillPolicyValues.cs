cc572dc3008a58ebf453fbe06a66d7c58adbfef1
vadim <vadim.mingazhev@gmail.com>
2022-02-21 00:28:33 +0500

    Fill markup values from settings

---
+++ HappyTravel.Edo.Data/Migrations/20220220192545_FillPolicyValues.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FillPolicyValues : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var fillFactorSql = "update \"MarkupPolicies\" set \"Value\" = \"TemplateSettings\"->'factor',  \"FunctionType\" = 1 where \"TemplateSettings\"->'factor' is not null;";
+            migrationBuilder.Sql(fillFactorSql);
+
+            var fillAdditionSql =
+                "update \"MarkupPolicies\" set \"Value\" = \"TemplateSettings\"->'addition', \"FunctionType\" = 2 where \"TemplateSettings\"->'addition' is not null;";
+
+            migrationBuilder.Sql(fillAdditionSql);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
