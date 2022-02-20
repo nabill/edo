6ce607dc330c36bc7eee88746d0333bfc0cf095e
vadim <vadim.mingazhev@gmail.com>
2022-02-20 22:32:47 +0500

    Using supplier codes instead of supplier ids in booking management

--- Api/Services/Accommodations/Bookings/BookingExecution/BookingRequestExecutor.cs
+++ Api/Services/Accommodations/Bookings/BookingExecution/BookingRequestExecutor.cs
@@ -92,7 +92,7 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Bookings.BookingExecution
                 try
                 {
                     var (isSuccess, _, bookingResult, error) = await TimeObserver.Execute(observedFunc: () => _supplierConnectorManager
-                        .Get(booking.Supplier)
+                        .GetByCode(booking.SupplierCode)
                         .Book(innerRequest, languageCode),
                         notifyFunc: Notify,
                         notifyAfter: TimeSpan.FromSeconds(BookExecutionTimeLimitInSeconds));
