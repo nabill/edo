d66864321ce15d837a51db7a51bf82d6e0e75ac3
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-02-25 21:12:11 +0600

    Removed unused SuccessfulPaymentReceipt template for admins

---
+++ HappyTravel.Edo.Data/Migrations/20220225150556_RemoveUnusedTemplate.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class RemoveUnusedTemplate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData("DefaultNotificationOptions", "Type", 4, "AdminEmailTemplateId", null); // SuccessfulPaymentReceipt
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
