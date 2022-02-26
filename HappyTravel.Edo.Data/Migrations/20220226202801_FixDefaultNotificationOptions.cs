7c7ab464c3753ccdc3b5599b2e16ccd50f9757b0
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-02-27 03:18:59 +0600

    Fixed migration

--- HappyTravel.Edo.Data/Migrations/20220226202801_FixDefaultNotificationOptions.cs
+++ HappyTravel.Edo.Data/Migrations/20220226202801_FixDefaultNotificationOptions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Migrations;
+﻿using HappyTravel.Edo.Notifications.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
 
 #nullable disable
 
@@ -9,9 +10,12 @@ namespace HappyTravel.Edo.Data.Migrations
         protected override void Up(MigrationBuilder migrationBuilder)
         {
             // AccountBalanceManagementNotification
-            migrationBuilder.UpdateData("DefaultNotificationOptions", "Type", 33, "EnabledReceivers", 1);
+            migrationBuilder.UpdateData("DefaultNotificationOptions", "Type", 33, "EnabledReceivers", (int)ReceiverTypes.AdminPanel);
             migrationBuilder.UpdateData("DefaultNotificationOptions", "Type", 33, "AgentEmailTemplateId", null);
             migrationBuilder.UpdateData("DefaultNotificationOptions", "Type", 33, "AdminEmailTemplateId", "d-33da6c91aaef4b86bfa3ebba68f9d4bc");
+
+            migrationBuilder.Sql("update \"AdministratorRoles\" set \"NotificationTypes\" = '{4, 19, 22, 33}' where \"Id\" = 2");
+            migrationBuilder.Sql("update \"AdministratorRoles\" set \"NotificationTypes\" = '{2, 6, 7, 18, 23, 34}' where \"Id\" = 3");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
