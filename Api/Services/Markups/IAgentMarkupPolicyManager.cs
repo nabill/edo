fde78712f4e4fa8db776cc4ef7a74f95306a5ac0
mingazhev <43397444+mingazhev@users.noreply.github.com>
2022-03-04 15:58:55 +0500

    Changed agent markup management

--- Api/Services/Markups/IAgentMarkupPolicyManager.cs
+++ Api/Services/Markups/IAgentMarkupPolicyManager.cs
@@ -1,19 +1,16 @@
-using System.Collections.Generic;
 using System.Threading.Tasks;
 using CSharpFunctionalExtensions;
 using HappyTravel.Edo.Api.Models.Agents;
-using HappyTravel.Edo.Api.Models.Markups;
+using HappyTravel.Edo.Api.Models.Markups.Agent;
 
 namespace HappyTravel.Edo.Api.Services.Markups
 {
     public interface IAgentMarkupPolicyManager
     {
-        Task<Result> Add(int agentId, MarkupPolicySettings policyData, AgentContext agent);
+        Task<Result> Set(int agentId, SetAgentMarkupRequest request, AgentContext agent);
 
-        Task<Result> Remove(int agentId, int policyId, AgentContext agent);
+        Task<Result> Remove(int agentId, AgentContext agent);
 
-        Task<Result> Modify(int agentId, int policyId, MarkupPolicySettings settings, AgentContext agent);
-
-        Task<List<MarkupInfo>> Get(int agentId, int agencyId);
+        Task<AgentMarkupInfo?> Get(int agentId, AgentContext agent);
     }
 }
--- Api/Services/Markups/Templates/IMarkupPolicyTemplateService.cs
+++ Api/Services/Markups/Templates/IMarkupPolicyTemplateService.cs
@@ -1,15 +1,12 @@
 using System;
 using System.Collections.Generic;
 using CSharpFunctionalExtensions;
-using HappyTravel.Edo.Api.Models.Markups.Templates;
 using HappyTravel.Edo.Data.Markup;
 
 namespace HappyTravel.Edo.Api.Services.Markups.Templates
 {
     public interface IMarkupPolicyTemplateService
     {
-        IReadOnlyCollection<MarkupPolicyTemplate> Get();
-
         Func<decimal, decimal> CreateFunction(int templateId, IDictionary<string, decimal> settings);
 
         Result Validate(int templateId, IDictionary<string, decimal> settings);
