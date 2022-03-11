5eaf0414310c0fbf2f407335245298d7ed837d88
Denis Nikolaev <denis.nikolaev@happytravel.com>
2022-03-11 08:38:11 +0500

    added range limit to markup percents

--- Api/Models/Markups/Agent/SetAgentMarkupRequest.cs
+++ Api/Models/Markups/Agent/SetAgentMarkupRequest.cs
@@ -1,6 +1,9 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace HappyTravel.Edo.Api.Models.Markups.Agent;
 
 public readonly struct SetAgentMarkupRequest
 {
+    [Range(0.1d, 100d)]
     public decimal Percent { get; init; }
 }
