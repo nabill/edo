2e394688bb588c3b27b2b99b13b54d2773c22e23
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-03-19 21:46:43 +0600

    Small refactoring

--- Api/Models/Company/CompanyInfo.cs
+++ Api/Models/Company/CompanyInfo.cs
@@ -4,8 +4,8 @@ namespace HappyTravel.Edo.Api.Models.Company
     {
         public string Name { get; set; }
         public string Address { get; set; }
-        public string City { get; set; }
         public string Country { get; set; }
+        public string City { get; set; }
         public string Phone { get; set; }
         public string Email { get; set; }
         public string PostalCode { get; set; }
