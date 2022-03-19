59e4229d1f38fa1abbe4d215342039d208340678
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-03-19 21:43:33 +0600

    Some rafactoring

--- Api/Models/Company/IntermediaryBank.cs
+++ Api/Models/Company/IntermediaryBank.cs
@@ -1,10 +1,9 @@
-﻿namespace HappyTravel.Edo.Api.Models.Company
+﻿namespace HappyTravel.Edo.Api.Models.Company;
+
+public record IntermediaryBank
 {
-    public record IntermediaryBank
-    {
-        public string BankName { get; init; }
-        public string AccountNumber { get; init; }
-        public string SwiftCode { get; init; }
-        public string AbaNo { get; init; }
-    }
+    public string BankName { get; init; }
+    public string AccountNumber { get; init; }
+    public string SwiftCode { get; init; }
+    public string AbaNo { get; init; }
 }
