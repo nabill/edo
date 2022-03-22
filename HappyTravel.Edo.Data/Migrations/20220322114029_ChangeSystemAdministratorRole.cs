14d01c63baa4250b74caaf48a7bd9cdee9b10a7f
Konstantin Yefremov <konstyefremov@gmail.com>
2022-03-22 15:54:21 +0400

    Add administrator management and markup management to system administrator role

---
+++ HappyTravel.Edo.Data/Migrations/20220322114029_ChangeSystemAdministratorRole.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using HappyTravel.Edo.Common.Enums.Administrators;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class ChangeSystemAdministratorRole : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Add AdministratorManagement and MarkupManagement permission to System Administrator role
+            migrationBuilder.Sql("update \"AdministratorRoles\" set \"Permissions\" = \"Permissions\" | 131072 | 8" +
+                " where \"Name\" = 'System Administrator';");
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
