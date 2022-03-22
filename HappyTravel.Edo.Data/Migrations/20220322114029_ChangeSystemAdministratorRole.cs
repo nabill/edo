793a923fb6509038d7fd9a04ad756892b8644df1
Konstantin Yefremov <konstyefremov@gmail.com>
2022-03-22 18:34:27 +0400

    Add notification types to super admin role

--- HappyTravel.Edo.Data/Migrations/20220322114029_ChangeSystemAdministratorRole.cs
+++ HappyTravel.Edo.Data/Migrations/20220322114029_ChangeSystemAdministratorRole.cs
@@ -12,7 +12,10 @@ namespace HappyTravel.Edo.Data.Migrations
             // Add AdministratorManagement and MarkupManagement permission to System Administrator role
             migrationBuilder.Sql("update \"AdministratorRoles\" set \"Permissions\" = \"Permissions\" | 131072 | 8" +
                 " where \"Name\" = 'System Administrator';");
-
+                
+            // Add notification types to super admin role
+            migrationBuilder.Sql("update \"AdministratorRoles\" set \"NotificationTypes\" = '{2,6,7,16,17,18,19,22,23,33,34}'" +
+                " where \"Name\" = 'Super-admin';");
         }
 
         protected override void Down(MigrationBuilder migrationBuilder)
