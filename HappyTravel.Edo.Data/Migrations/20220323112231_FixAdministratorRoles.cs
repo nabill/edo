a2d7dfc66058544e49c925d81a7b32331b636c2a
Konstantin Yefremov <konstyefremov@gmail.com>
2022-03-23 15:28:21 +0400

    Delete reports manager role and rename auditor to report manager

---
+++ HappyTravel.Edo.Data/Migrations/20220323112231_FixAdministratorRoles.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FixAdministratorRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("delete from \"AdministratorRoles\" where \"Name\" = 'Reports manager';");
+            migrationBuilder.Sql("update \"AdministratorRoles\" set \"Name\" = 'Report manager' where \"Name\" = 'Auditor';");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
