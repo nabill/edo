f615de849fd8aa7e542239bd423fc330d1772a43
AlexeyShok <Alexey.Shokarev@gmail.com>
2022-03-25 14:37:10 +0600

    Fixed some main models

--- HappyTravel.Edo.Data/Markup/MarkupPolicy.cs
+++ HappyTravel.Edo.Data/Markup/MarkupPolicy.cs
@@ -7,14 +7,14 @@ namespace HappyTravel.Edo.Data.Markup
     public class MarkupPolicy
     {
         public int Id { get; set; }
-        public string Description { get; set; }
+        public string? Description { get; set; }
         public DateTimeOffset Created { get; set; }
         public DateTimeOffset Modified { get; set; }
         public Currencies Currency { get; set; }
         public SubjectMarkupScopeTypes SubjectScopeType { get; set; }
-        public string SubjectScopeId { get; set; }
+        public string? SubjectScopeId { get; set; }
         public DestinationMarkupScopeTypes DestinationScopeType { get; set; }
-        public string DestinationScopeId { get; set; }
+        public string? DestinationScopeId { get; set; }
         public MarkupFunctionType FunctionType { get; set; }
         public decimal Value { get; set; }
     }
