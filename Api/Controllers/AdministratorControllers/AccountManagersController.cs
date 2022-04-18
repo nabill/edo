1a8ac0b1353ea50745926aa968fa00557c73182d
Daniyar Askarov <daniyar.ozat.97@outlook.com>
2022-04-18 15:24:35 +0300

    Add service and endpoint for setting account manager to agency

--- Api/Controllers/AdministratorControllers/AccountManagersController.cs
+++ Api/Controllers/AdministratorControllers/AccountManagersController.cs
@@ -6,6 +6,7 @@ using HappyTravel.Edo.Api.Controllers;
 using HappyTravel.Edo.Api.Filters.Authorization.AdministratorFilters;
 using HappyTravel.Edo.Api.Models.Management.Administrators;
 using HappyTravel.Edo.Common.Enums.Administrators;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Api.Controllers.AdministratorControllers
@@ -27,11 +28,23 @@ namespace Api.Controllers.AdministratorControllers
         /// </summary>
         /// <returns>List of account managers' info</returns>
         [HttpGet("account-managers")]
-        [ProducesResponseType(typeof(List<AdministratorInfo>), (int)HttpStatusCode.OK)]
-        [ProducesResponseType(typeof(ProblemDetails), (int)HttpStatusCode.BadRequest)]
+        [ProducesResponseType(typeof(List<AdministratorInfo>), StatusCodes.Status200OK)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
         [AdministratorPermissions(AdministratorPermissions.AdministratorManagement)]
         public async Task<IActionResult> Get()
             => Ok(await _administratorManagementService.GetAccountManagers());
+
+
+        /// <summary>
+        ///     Add account manager to agency
+        /// </summary>
+        /// <returns></returns>
+        [HttpGet("agencies/{agencyId}/account-manager")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+        [AdministratorPermissions(AdministratorPermissions.AdministratorManagement)]
+        public async Task<IActionResult> Add([FromRoute] int agencyId, [FromQuery] int? accountManagerId)
+            => NoContentOrBadRequest(await _administratorManagementService.AddAccountManager(agencyId, accountManagerId));
 
 
         private readonly IAdministratorManagementService _administratorManagementService;
