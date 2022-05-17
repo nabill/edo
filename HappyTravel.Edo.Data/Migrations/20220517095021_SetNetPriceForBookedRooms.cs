cd7b565b17b3f565e307ae16bf07a32e9425bd92
Daniyar Askarov <daniyar.ozat.97@outlook.com>
2022-05-17 14:43:26 +0300

    Migration was added

---
+++ HappyTravel.Edo.Data/Migrations/20220517095021_SetNetPriceForBookedRooms.cs
@@ -0,0 +1,39 @@
+﻿using System.Linq;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class SetNetPriceForBookedRooms : Migration
+    {
+        public SetNetPriceForBookedRooms(EdoContext context)
+        {
+            _context = context;
+        }
+
+
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var bookings = _context.Bookings.ToList();
+            bookings.ForEach(b =>
+            {
+                b.Rooms.ForEach(r =>
+                {
+                    r.NetPrice = r.Price;
+                });
+            });
+
+            _context.UpdateRange(bookings);
+            _context.SaveChanges();
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+
+        private readonly EdoContext _context;
+    }
+}
