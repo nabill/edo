9137c2dc01b4204920641307b4fb7b6304ce9b28
Daniyar Askarov <daniyar.ozat.97@outlook.com>
2022-06-17 18:10:14 +0900

    Migration for fullfilling creditCardPrice field in old bookings

---
+++ HappyTravel.Edo.Data/Migrations/20220617074229_FulfillCreditCardPriceFieldInBookings.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class FulfillCreditCardPriceFieldInBookings : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            var context = new EdoContextFactory().CreateDbContext(Array.Empty<string>());
+            var bookings = context.Bookings.ToList();
+            bookings.ForEach(b =>
+                {
+                    b.Rooms.ForEach(r =>
+                    {
+                        r.CreditCardPrice = r.Price;
+                    });
+                }
+            );
+
+            context.UpdateRange(bookings);
+            context.SaveChanges();
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
