a96c49a168f425c1248c2e10baae0079998cf342
Denis Nikolaev <dfs.str@gmail.com>
2022-07-01 07:02:21 +0500

    added descriptions for public properties

--- Api/Models/Messaging/MailMessage.cs
+++ Api/Models/Messaging/MailMessage.cs
@@ -5,7 +5,18 @@ namespace HappyTravel.Edo.Api.Models.Messaging;
 
 public class MailMessage
 {
+    /// <summary>
+    ///     SendGrid template Id
+    /// </summary>
     public string TemplateId { get; set; } = string.Empty;
+    
+    /// <summary>
+    ///     Recipients e-mail adresses
+    /// </summary>
     public IEnumerable<string> Recipients { get; set; } = Array.Empty<string>();
+    
+    /// <summary>
+    ///     E-mail data
+    /// </summary>
     public object? Data { get; set; }
 }
