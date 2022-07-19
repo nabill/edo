47685e798871546bbc4f1b569d16b391c2bf81fa
timur-mut <timur-mut@live.com>
2022-07-19 12:00:22 +0400

    Deploy fix

--- Api/Infrastructure/ServiceCollectionExtensions.cs
+++ Api/Infrastructure/ServiceCollectionExtensions.cs
@@ -500,7 +500,7 @@ namespace HappyTravel.Edo.Api.Infrastructure
             services.AddTransient<IBookingRecordManager, BookingRecordManager>();
             services.AddTransient<ITagProcessor, TagProcessor>();
             
-            services.AddSingleton<IBookingPhotoLoadingService, BookingPhotoLoadingService>();
+            services.AddTransient<IBookingPhotoLoadingService, BookingPhotoLoadingService>();
             services.AddSingleton<ITokenInfoAccessor, TokenInfoAccessor>();
             services.AddSingleton<IIdentityUserInfoService, IdentityUserInfoService>();
             services.AddTransient<IAccountBalanceAuditService, AccountBalanceAuditService>();
