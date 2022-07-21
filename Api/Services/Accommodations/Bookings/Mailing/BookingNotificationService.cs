78f310c4926dd523f6a63e019ba0878a9aad8460
Daniyar Askarov <daniyar.ozat.97@outlook.com>
2022-07-21 08:32:37 +0300

    Change method back for check in and check out

--- Api/Services/Accommodations/Bookings/Mailing/BookingNotificationService.cs
+++ Api/Services/Accommodations/Bookings/Mailing/BookingNotificationService.cs
@@ -111,8 +111,8 @@ namespace HappyTravel.Edo.Api.Services.Accommodations.Bookings.Mailing
                         RoomDescriptions = string.Join(", ", roomDescriptions),
                         Passengers = string.Join(", ", passengers),
                         ReferenceCode = booking.ReferenceCode,
-                        CheckInDate = DateTimeFormatters.ToUtcString(booking.CheckInDate),
-                        CheckOutDate = DateTimeFormatters.ToUtcString(booking.CheckOutDate),
+                        CheckInDate = DateTimeFormatters.ToDateString(booking.CheckInDate),
+                        CheckOutDate = DateTimeFormatters.ToDateString(booking.CheckOutDate),
                         Deadline = DateTimeFormatters.ToUtcString(booking.DeadlineDate!.Value),
                         OfflineNotificationsType = notificationType
                     };
