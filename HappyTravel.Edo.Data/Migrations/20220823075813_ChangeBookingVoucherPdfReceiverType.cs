b9d991e428b6c66a1d935b8cf7c27b262daa6756
Daniyar Askarov <daniyar.ozat.97@outlook.com>
2022-08-23 14:03:35 +0600

    Change EnabledReceivers to Admin and Agent panels

---
+++ HappyTravel.Edo.Data/Migrations/20220823075813_ChangeBookingVoucherPdfReceiverType.cs
@@ -0,0 +1,24 @@
+﻿using HappyTravel.Edo.Notifications.Enums;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HappyTravel.Edo.Data.Migrations
+{
+    public partial class ChangeBookingVoucherPdfReceiverType : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData("DefaultNotificationOptions",
+                "Type",
+                (int)NotificationTypes.BookingVoucherPdf,
+                "EnabledReceivers",
+                (int)(ReceiverTypes.AdminPanel | ReceiverTypes.AgentApp));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
